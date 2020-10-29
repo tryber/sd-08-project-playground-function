08d5f431b8da2cf43d3af18fa64b37c92c77e786
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-10-29 09:01:12 -0300

    add requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,85 +1,98 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(num1,num2,num3) {
+  
+  let a = num1;
+  let b = num2;
+  let c = num3;
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+  if (a<b == true && a<c == true){
+  console.log(num1)
+  } else if (b<a == true && b<c == true){
+    console.log(num2)
+  } else if (c<a == true && c<b == true){
+    console.log(num3)
+  } else {
+    console.log("Há números iguais")
+  }    
+} compareTrue(19,20,7)
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+// // Desafio 2
+// function calcArea() {
+//   // seu código aqui
+// }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// // Desafio 3
+// function splitSentence() {
+//   // seu código aqui
+// }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
+
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
