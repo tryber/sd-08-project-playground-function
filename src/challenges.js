3738afab5cae24bb31bff9f17cb79195b93f04e7
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-10-29 14:59:52 -0300

    1 - Usando o operador &&

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(booleanOne, booleanTwo) {
+  if(booleanOne && booleanTwo){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 2
