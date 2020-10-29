a078a292f0c688e80854e77536ea45378fdd7fbf
Douglas Fernando <doug.fernando95@gmail.com>
2020-10-29 16:25:41 -0300

    Funçao: calcArea()

--- src/challenges.js
+++ src/challenges.js
@@ -8,9 +8,11 @@ function compareTrue(a,b) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
+
 
 // Desafio 3
 function splitSentence() {
