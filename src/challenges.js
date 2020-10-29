895434014d4ec5c841b9857db22e0c0ec5ff1295
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-10-29 15:26:39 -0300

    Desafio 04 - COMPLETADO!

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,8 @@ function splitSentence(phrase) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayOfStrings) {
+  return arrayOfStrings[0] + ", " + arrayOfStrings[arrayOfStrings.length-1];
 }
 
 // Desafio 5
