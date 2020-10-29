102112a772cbfcecd251899ed48a0fcffec3cab8
siebenei5 <siebenei7@gmail.com>
2020-10-29 14:50:19 -0300

    Test Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,13 +9,13 @@ function compareTrue(a, b)
 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height)
+{
+  return base * height / 2;
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
 }
 
 // Desafio 4
