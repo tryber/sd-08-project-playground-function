6a2c52108f1636669b156e5f2bc4f9d2a94711eb
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-10-29 15:14:39 -0300

    4 - Concatenação de strings

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let concat = array[array.length-1] + ", " + array[0];
+  return concat;
 }
 
 // Desafio 5
