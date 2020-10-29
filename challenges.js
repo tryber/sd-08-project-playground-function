869d9f15f0caea49277400ca57ce318fc5af00c2
emassaki <massakihirayama@gmail.com>
2020-10-29 10:46:18 -0300

    Adicionando requisito 3

--- challenges.js
+++ challenges.js
@@ -1,17 +1,26 @@
 //Requisito 1 - Usando o operador &&
 function compareTrue(boolean1, boolean2) {
   if (boolean1 == true && boolean2 == true) {
+    console.log(true);
     return true;
   } else {
+    console.log(false);
     return false;
   }
 }
-
-// console.log(compareTrue(true, true));
+compareTrue(false, false);
 
 //Requisito 2 - Área do triângulo
 function calcArea(base, height) {
+  console.log((base * height) / 2);
   return (base * height) / 2;
 }
+calcArea(10, 20);
 
-// console.log(calcArea(7, 15));
+//Requisito 3 - Dividindo a frase
+function splitSentence (sentence) {
+  console.log(sentence.split(" "));
+  return sentence.split(" ");
+}
+
+splitSentence('go Trybe')
