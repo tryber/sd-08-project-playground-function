7e059403e962158854351b03993a70bb936f7fc5
Fabio Ferreira <fabio35547@gmail.com>
2020-10-29 14:57:23 -0300

    Desafio 4 - concluido

--- src/challenges.js
+++ src/challenges.js
@@ -28,9 +28,11 @@ function splitSentence(texto) {
 //console.log(splitSentence("go Trybe"));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ', ' + array[0];
 }
+
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
 function footballPoints() {
