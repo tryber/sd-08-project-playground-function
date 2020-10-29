f2ebc6f88382a666ef7d667212d16d01b5353a7b
Thais Quintela <thais.s.quintela@gmail.com>
2020-10-29 18:41:39 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -16,7 +16,7 @@ function calcArea(base, height) {
 function splitSentence(sentence) {
   return sentence.split(' ');
 
-// Usei o método split que relembrei em https://www.w3schools.com/jsref/jsref_split.asp#:~:text=The%20split()%20method%20is,not%20change%20the%20original%20string.
+// Usei o método split que relembrei em https://www.w3schools.com/jsref/jsref_split.asp#:~:text=The%20split()%20method%20is,not%20change%20the%20original%20string. , esse método corta a string sempre que passa pelo valor especificado (nesse caso um espaço) e ordena os "pedaços" em um array.
 }
 
 // Desafio 4
@@ -30,28 +30,40 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(numbers) {
-  
-   
+function highestCount() {
+  //
 }
 
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let win;
-  if (cat1 - mouse == cat2 - mouse) {
-    win = "os gatos trombam e o rato foge";
-  } else if (cat1 - mouse < cat2 - mouse){
-    win = "cat1";
-  } else if (cat1 - mouse > cat2 - mouse) {
-    win = "cat2";
+  if (Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)) {
+    win = 'os gatos trombam e o rato foge';
+  } else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    win = 'cat1';
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    win = 'cat2';
   }
   return win;
+// Usei a função de valor absoluto que aprendi nesse site https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs , essa função retorna o módulo do valor do input.
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let answers = [];
+  for (let index in numbers) {
+    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      answers.push('fizzBuzz');
+    } else if (numbers[index] % 3 == 0) {
+      answers.push('fizz');
+    } else if (numbers[index] % 5 == 0) {
+      answers.push('buzz');
+    } else {
+      answers.push('bug!');
+    }
+  }
+  return answers;
 }
 
 // Desafio 9
