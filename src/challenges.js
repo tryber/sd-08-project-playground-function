d5e791db2d03840fb244358e91b7e6b5f8235847
Aline Debastiani <debastianialine@gmail.com>
2020-10-29 14:56:15 -0300

    cumpre desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if ( a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(compareTrue(2, 4));
 
 // Desafio 2
 function calcArea() {
