44ee293738e1bb5b4aac7eaece235915b23802b0
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-10-29 16:20:53 -0300

    Exercício 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1,value2) {
+  return value1 &&value2;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base,height) {
   let area = (base*altura)/2;
   return area;
 }
