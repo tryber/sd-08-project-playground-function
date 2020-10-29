6e5c52c7037caca853a3f171edd7d74de8804b7c
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-29 16:07:37 -0300

    exercicio 3 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -19,8 +19,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string ) {
+  
+  return string.split(" ", string.length)
 }
 
 // Desafio 4
