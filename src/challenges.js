b00b678da915a9bb056a27eabff9b4a3b21168c2
Paulo Simões <paulohbsimoes@gmail.com>
2020-10-29 18:13:00 -0300

    Consertando bugs no requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -37,7 +37,7 @@ function highestCount(numbers) {
     }
   }
 
-  let highest = Math.max(numbers);
+  let highest = Math.max(...numbers);
   return counter[highest];
 }
 
@@ -184,10 +184,6 @@ function generatePhoneNumber(digits) {
 
   return `(${ddd}) ${partA}-${partB}`;
 }
-
-console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4])); //'não é possível gerar um número de telefone com esses valores');
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])); //(12) 34567-8901
-
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
