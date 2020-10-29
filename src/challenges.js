f9148ba32a941daf6d1735da9a3b3f3b399d2883
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-10-29 15:49:35 -0300

    starting the project let's go with everything

--- src/challenges.js
+++ src/challenges.js
@@ -1,85 +1,45 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+//Desafio 1
+let value1 = 5;
+let value2 = 10;
+function compareTrue(value1, value2) {
+  if( value1 === 5 && value2 === 10) {
+    return true
+    
+  }else{
+    return false
+  }
+     
+  }
+
+console.log(compareTrue(value1,value2));
 
 // Desafio 2
 function calcArea() {
-  // seu código aqui
-}
-
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
-
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
-
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
-
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
-
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
-
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
-
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+    // seu código aqui
+  }
+  
+  // Desafio 3
+  function splitSentence() {
+    // seu código aqui
+  }
+  
+  // Desafio 4
+  function concatName() {
+    // seu código aqui
+  }
+  
+  // Desafio 5
+  function footballPoints() {
+    // seu código aqui
+  }
+  
+  // Desafio 6
+  function highestCount() {
+    // seu código aqui
+  }
+  
+  // Desafio 7
+  function catAndMouse() {
+    // seu código aqui
+  }
+  
