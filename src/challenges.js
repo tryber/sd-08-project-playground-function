c7874d0aaedd2509d0863d68270a4d6f6283361c
Felipe Franca <felipefrancafj@gmail.com>
2020-10-29 16:34:57 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,10 +14,10 @@ function calcArea(base,height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let arrayFrase = string.split(' ')
+  return arrayFrase
 }
-
 // Desafio 4
 function concatName() {
   // seu código aqui
