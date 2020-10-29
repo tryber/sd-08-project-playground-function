8ff757d6a374e95db6e8aa42c1f1f2cb958d1458
Tiago Bovolin <tiagaoalb@gmail.com>
2020-10-29 19:17:17 -0300

    added eighth requirement

--- src/challenges.js
+++ src/challenges.js
@@ -71,10 +71,22 @@ function catAndMouse(mouse, cat1, cat2) {
 catAndMouse(1, 0, 2);
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      result.push("fizzBuzz");
+    } else if (array[i] % 3 === 0) {
+      result.push("fizz");
+    } else if (array[i] % 5 === 0) {
+      result.push("buzz");
+    } else {
+      result.push("bug!")
+    }
+  }
+  return result;
 }
-
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
