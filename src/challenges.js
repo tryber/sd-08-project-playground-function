1aea0f5296d558a789caa77846b83f0988c4bfa7
Tandy <tandyfw@gmail.com>
2020-10-29 16:52:47 -0300

    Correcao desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -70,9 +70,9 @@ console.log(highestCount(numeros));
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if(cat1 < cat2){
+  if((cat1 - mouse) < (cat2 - mouse)){
     return cat1;
-  }else if(cat2 < cat1){
+  }else if((cat1 - mouse) > (cat2 - mouse)){
     return cat2;
   } else{
     return "os gatos trombam e o rato foge";
