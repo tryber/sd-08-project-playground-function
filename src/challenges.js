b00e63b4475651d033f89a464e74983d951ac53f
Cristiano <cristianoroberto@outlook.com>
2020-10-29 19:24:20 -0300

    Desafio1 Correção &&

--- src/challenges.js
+++ src/challenges.js
@@ -1,97 +1,91 @@
 // Desafio 1
 function compareTrue(valueA, valueB) {
-    //[true]&&[true]  = [true]
-    //[true]&&[false] = [false]
-    //[false]&&[true] = [false]
-    //[false]&&[false]= [false]
 
-    let valueA;
-    let valueB;
-
-    if (valueA == true && valueB == true) {
-        return true
+    if (valueA && valueB) {
+        return true;
     } else {
-        return false
+        return false;
     }
+}
 
-    // Desafio 2
-    function calcArea(base, height) {
-        return (base * height) / 2
-    }
+// Desafio 2
+function calcArea(base, height) {
+    return (base * height) / 2
+}
 
-    // Desafio 3
-    function splitSentence() {
-        // seu código aqui
-    }
+// Desafio 3
+function splitSentence() {
+    // seu código aqui
+}
 
-    // Desafio 4
-    function concatName() {
-        // seu código aqui
-    }
+// Desafio 4
+function concatName() {
+    // seu código aqui
+}
 
-    // Desafio 5
-    function footballPoints() {
-        // seu código aqui
-    }
+// Desafio 5
+function footballPoints() {
+    // seu código aqui
+}
 
-    // Desafio 6
-    function highestCount() {
-        // seu código aqui
-    }
+// Desafio 6
+function highestCount() {
+    // seu código aqui
+}
 
-    // Desafio 7
-    function catAndMouse() {
-        // seu código aqui
-    }
+// Desafio 7
+function catAndMouse() {
+    // seu código aqui
+}
 
-    // Desafio 8
-    function fizzBuzz() {
-        // seu código aqui
-    }
+// Desafio 8
+function fizzBuzz() {
+    // seu código aqui
+}
 
-    // Desafio 9
-    function encode() {
-        // seu código aqui
-    }
+// Desafio 9
+function encode() {
+    // seu código aqui
+}
 
-    function decode() {
-        // seu código aqui
-    }
+function decode() {
+    // seu código aqui
+}
 
-    // Desafio 10
-    function techList() {
-        // seu código aqui
-    }
+// Desafio 10
+function techList() {
+    // seu código aqui
+}
 
-    // Desafio 11
-    function generatePhoneNumber() {
-        // seu código aqui
-    }
+// Desafio 11
+function generatePhoneNumber() {
+    // seu código aqui
+}
 
-    // Desafio 12
-    function triangleCheck() {
-        // seu código aqui
-    }
+// Desafio 12
+function triangleCheck() {
+    // seu código aqui
+}
 
-    // Desafio 13
-    function hydrate() {
-        // seu código aqui
-    }
+// Desafio 13
+function hydrate() {
+    // seu código aqui
+}
 
 
-    module.exports = {
-        calcArea,
-        catAndMouse,
-        compareTrue,
-        concatName,
-        decode,
-        encode,
-        fizzBuzz,
-        footballPoints,
-        generatePhoneNumber,
-        techList,
-        highestCount,
-        hydrate,
-        splitSentence,
-        triangleCheck,
-    }
+module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    generatePhoneNumber,
+    techList,
+    highestCount,
+    hydrate,
+    splitSentence,
+    triangleCheck,
+}
