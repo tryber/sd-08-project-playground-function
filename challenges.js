4db5be0a3446c1c8e374f628deacdd90ae20b33a
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-10-29 13:52:55 -0300

    Concatenação de strings

--- challenges.js
+++ challenges.js
@@ -1,6 +1,6 @@
 // 1 - Usando o operador &&
 
-function compareTrue (valor1, valor2){
+function compareTrue (valor1, valor2) {
     if (valor1 && valor2){
         return true;
     } else {
@@ -12,7 +12,7 @@ function compareTrue (valor1, valor2){
 
 // 2 - Área do triângulo
 
-function calcArea(base, height){
+function calcArea (base, height) {
     let area = base * height / 2;
     return area;
 }
@@ -21,9 +21,26 @@ function calcArea(base, height){
 
 // 3 - Dividindo a base
 
-function splitSentence(literal){
+function splitSentence (literal) {
     let split = literal.split(" ");
     return split;
 }
 
 //console.log(splitSentence("go Trybe"));
+
+// 4 - Concatenação de strings
+
+function concatName (array) {
+    let string;
+    for (i in array){
+        if(i == array.length - 1){
+            string = array[i];
+        }
+    }
+    if(array[0]){
+        string += ", " + array[0];
+    }
+    return string;
+}
+
+//console.log(concatName(["Lucas", "Cassiano", "Ferraz", "Paolillo"]))
