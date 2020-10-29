0827e4e99d9a5304cd20d2a00d71e7c8a6a31378
Lucas Duque <lucas3rios2014@gmail.com>
2020-10-29 14:37:53 -0300

    Consertando codeClimate desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,7 @@
 // Desafio 1
-function compareTrue(boolean1,boolean2) {
-  if(boolean1 == true && boolean2 == true) {
+function compareTrue(boolean1 , boolean2) {
+  if (boolean1 === true && boolean2 === true) {
     return true;
-  }
-  else {
-    return false;
   }
 }
 
