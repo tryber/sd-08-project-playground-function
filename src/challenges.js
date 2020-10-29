677b5ccdc7a3d2dce9c4344eaca8d0e19e19a997
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-10-29 15:48:00 -0300

    realizado requisito 01

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui .
+function compareTrue(valor1, valor2) {
+  // seu código aqui
+  if(valor1 === true && valor2 === true ) {
+      return true;
+  }else {
+    return false;
+  }
+  
 }
 
 // Desafio 2
