2a49fa38ee1e0961ea12dcb55b3bb7809476634a
Patrícia <silva.patricia.v@gmail.com>
2020-10-29 17:32:08 -0300

    Requirement 3 completed

--- src/challenges.js
+++ src/challenges.js
@@ -13,10 +13,10 @@ function calcArea(base, heigth) {
   return area;
 }
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  array = string.split(" ");
+  return array;
 }
-
 // Desafio 4
 function concatName() {
   // seu código aqui
