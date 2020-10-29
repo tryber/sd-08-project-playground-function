52cfd44f00f5ec08dedc35a6f4c4f705d8a307d2
emassaki <massakihirayama@gmail.com>
2020-10-29 10:08:09 -0300

    Adicionando requisito 1

---
+++ challenges.js
@@ -0,0 +1,10 @@
+//Requisito 1 - Usando o operador &&
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 == true && boolean2 == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(compareTrue(false,false))
