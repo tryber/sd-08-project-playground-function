c54d5834f779ca897906e6ee7ed2c7f1292bd913
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-10-29 16:45:10 -0300

    adjusting exercises 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,47 +1,44 @@
 //Desafio 1
-let value1 = 5;
-let value2 = 10;
+let value1 = true;
+let value2 = true;
 function compareTrue(value1, value2) {
-  if( value1 < value2) {
-    return true;
-  }else{
-    return false;
-  }   
-  }
+    if(value1 && value2 === true) {
+        return true;
+        } else {
+        return false
+        }
 
-console.log(compareTrue(value1,value2));
+        console.log(compareTrue(value1, value2));
 
 // Desafio 2
 
+function calcArea(base, height) {
+  return (base * height) / 2;
+}
 
-function calcArea(base,height) {
-    return (base * height)/2;
-  }
-   
-console.log(calcArea(10,5));
+console.log(calcArea(10, 5));
+
+// Desafio 3
+function splitSentence() {
   
-  // Desafio 3
-  function splitSentence() {
-    // seu código aqui
-  }
-  
-  // Desafio 4
-  function concatName() {
-    // seu código aqui
-  }
-  
-  // Desafio 5
-  function footballPoints() {
-    // seu código aqui
-  }
-  
-  // Desafio 6
-  function highestCount() {
-    // seu código aqui
-  }
-  
-  // Desafio 7
-  function catAndMouse() {
-    // seu código aqui
-  }
-  
+}
+
+// Desafio 4
+function concatName() {
+  // seu código aqui
+}
+
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
+
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
