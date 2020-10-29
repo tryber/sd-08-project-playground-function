b6e475088d335dc0197bf781018921cb64a116af
wagnerberna <cabelo29007@gmail.com>
2020-10-29 16:33:00 -0300

    Requisito 03 correção

--- src/challenges.js
+++ src/challenges.js
@@ -17,8 +17,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let resultado = [];
+  resultado = frase.split(" "); 
+  return resultado;
 }
 
 // Desafio 4
