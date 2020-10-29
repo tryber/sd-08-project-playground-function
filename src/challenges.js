f4bd704996adc71a88d179d21324fae947e11ea1
Marcela <marcela9908@gmail.com>
2020-10-29 14:49:22 -0300

    Adiciona Desafio 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,10 @@ function compareTrue(n1 , n2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  calcArea = (base * height) / 2;
+  return calcArea;
+
 }
 
 // Desafio 3
