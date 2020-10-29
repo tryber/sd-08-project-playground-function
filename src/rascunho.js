d07cb1b10c62321e4d93a0e3064467d07d8bbed3
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-29 14:40:52 -0300

    desafio 1 - teste

---
+++ src/rascunho.js
@@ -0,0 +1,12 @@
+function compareTrue(boolean1, boolean2) {
+    if(boolean1 == true && boolean2 == true){
+      return true;
+    }
+    else{
+        return false;
+    }
+    
+  }
+  console.log(compareTrue(true,true));
+  console.log(compareTrue(true,false))
+  console.log(compareTrue(false,false))
