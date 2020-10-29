3ccc9ded4257789cec0313281409c9ca2e051305
Luiz Reina <luizreeina@gmail.com>
2020-10-29 16:51:40 -0300

    d7 feito + climate

--- src/challenges.js
+++ src/challenges.js
@@ -68,8 +68,16 @@ function highestCount(array) {
 // console.log(highestCount(array))
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = mouse - cat1
+  let distancia2 = mouse - cat2
+
+  if (distancia1 < distancia2){
+    return "cat1"
+  } else if (distancia2 < distancia1){
+    return "cat2"
+  }
+  return "os gatos trombam e o rato foge"
 }
 
 // Desafio 8
