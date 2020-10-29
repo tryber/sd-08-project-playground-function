61fa96705070d463967fd00226df826d2713d54e
Pedro Amaral <amaralph1995@gmail.com>
2020-10-29 18:48:13 -0300

    Corrige o código pra passar no codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -5,18 +5,17 @@ function compareTrue(a, b) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height) / 2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  let saida = array[array.length - 1] + ", " + array[0];
-  return saida;
+  return `${array[array.length - 1]},  ${array[0]}`;
 }
 
 // Desafio 5
@@ -26,13 +25,13 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  array.sort(function(a, b) {return a-b})
+  array.sort(function (a, b) { return a - b; })
   let cont = 0;
 
   for (let i = array.length - 1; i >= 0; i -= 1) {
-    if (i == array.length - 1) {
+    if (i === array.length - 1) {
       cont += 1;
-    } else if (array[i] == array[i+1]) {
+    } else if (array[i] === array[i + 1]) {
       cont += 1;
     } else {
       break;
@@ -46,73 +45,70 @@ function catAndMouse(mouse, cat1, cat2) {
   let d1 = Math.abs(mouse - cat1);
   let d2 = Math.abs(mouse - cat2);
 
-  if (d1 == d2) {
-    return "os gatos trombam e o rato foge";
-  } else {
-    return d1 > d2 ? "cat2" : "cat1";
-  };
+  if (d1 === d2) {
+    return 'os gatos trombam e o rato foge';
+  } 
+  return d1 > d2 ? 'cat2' : 'cat1';
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let saida = [];
-
-  for (let i in array) {
-    let x = array[i]
-    if (x % 3 == 0 && x % 5 == 0) {
-      saida.push("fizzBuzz");
-    } else if (x % 3 == 0) {
-      saida.push("fizz");
-    } else if (x % 5 == 0) {
-      saida.push("buzz");
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      saida.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+      saida.push('fizz');
+    } else if (array[i] % 5 === 0) {
+      saida.push('buzz');
     } else {
-      saida.push("bug!");
-    };
-  };
+      saida.push('bug!');
+    }
+  }
   return saida;
 }
 
 // Desafio 9
 function encode(string) {
   let dicionario = {
-    "a": 1,
-    "e": 2,
-    "i": 3,
-    "o": 4,
-    "u": 5
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5
   };
 
-  let saida = "";
+  let saida = '';
 
-  for (let i in string) {
-    if (dicionario[string[i]] == undefined) {
+  for (let i = 0; i < string.length; i += 1) {
+    if (dicionario[string[i]] === undefined) {
       saida += string[i];
     } else {
       saida += dicionario[string[i]];
-    };
-  };
+    }
+  }
   return saida;
 }
 
 function decode(string) {
   let dicionario = {
-    "1": "a",
-    "2": "e",
-    "3": "i",
-    "4": "o",
-    "5": "u"
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u'
   };
 
-  let saida = "";
+  let saida = '';
 
-  for (let i in string) {
+  for (let i = 0; i < string.length; i += 1) {
     let x = string[i];
-    if (dicionario[x] == undefined) {
+    if (dicionario[x] === undefined) {
       saida += x;
     } else {
       saida += dicionario[x];
-    };
-  ;}
+    }
+  }
   return saida;
 }
 
@@ -120,36 +116,35 @@ function decode(string) {
 function techList(array, name) {
   let saida = [];
 
-  if (array.length == 0) {
-    return "Vazio!"
+  if (array.length === 0) {
+    return 'Vazio!';
   }
 
-  for (let i in array) {
+  for (let i = 0; i < array.length; i += 1) {
     let obj = {};
     obj.tech = array[i];
     obj.name = name;
     saida.push(obj);
-  };
-
+  }
   return saida;
 }
 
 // Desafio 11
 function generatePhoneNumber(array) {
   if (array.length != 11) {
-    return "Array com tamanho incorreto.";
+    return 'Array com tamanho incorreto.';
   }
 
-  let saida = "(";
+  let saida = '(';
 
   for (let i in array) {
     if (i == 2) {
-      saida += ") ";
+      saida += ') ' ;
     } else if (i == 7) {
-      saida += "-";
+      saida += '-';
     }
     saida += array[i];
-  };
+  }
   return saida;
 }
 
@@ -163,20 +158,19 @@ function triangleCheck(lineA, lineB, lineC) {
     return false;
   } else {
     return true;
-  };
+  }
 }
 
 // Desafio 13
 function hydrate(string) {
-  let dados = string.split(" ").sort();
+  let dados = string.split(' ').sort();
   let saida = 0;
 
-  for (let i in dados) {
+  for (let i = 0; i < dados.length; i += 1) {
     if (!isNaN(Number(dados[i]))) {
-      saida += Number(dados[i])
+      saida += Number(dados[i]);
     }
-  };
-
+  }
   return saida;
 }
 
