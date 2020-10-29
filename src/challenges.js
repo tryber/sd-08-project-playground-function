397bb53631b531152ce67133e806bcea9eea36a3
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2020-10-29 14:28:12 -0400

    Desafio 03

--- src/challenges.js
+++ src/challenges.js
@@ -8,72 +8,103 @@ function compareTrue(valor1, valor2) {
 }
 console.log(compareTrue(true, false))
 
+//---------------------------------//
+
 // Desafio 2
 function calcArea(base, height) {
 
   return (base * height) / 2
 
 }
-
 console.log(calcArea(10, 10))
 
+//---------------------------------//
+
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  resultado = frase.split(" ")
+
+  return resultado
 }
+console.log(splitSentence("Ola mundo"))
+
+//---------------------------------//
 
 // Desafio 4
 function concatName() {
   // seu código aqui
 }
 
+//---------------------------------//
+
 // Desafio 5
 function footballPoints() {
   // seu código aqui
 }
+
+//---------------------------------//
 
 // Desafio 6
 function highestCount() {
   // seu código aqui
 }
 
+//---------------------------------//
+
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
 }
+
+//---------------------------------//
 
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
 }
 
+//---------------------------------//
+
 // Desafio 9
 function encode() {
   // seu código aqui
 }
+
+//---------------------------------//
+
 function decode() {
   // seu código aqui
 }
+
+//---------------------------------//
 
 // Desafio 10
 function techList() {
   // seu código aqui
 }
 
+//---------------------------------//
+
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
 }
+
+//---------------------------------//
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
 }
 
+//---------------------------------//
+
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
+
+//---------------------------------//
 
 
 module.exports = {
