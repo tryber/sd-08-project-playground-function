ae559c1e52a9c2552eb7d485c3cb5a610f93afb0
Ruben Albertassi <albertassi88@gmail.com>
2020-10-29 17:20:08 -0300

    Inclusao do exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -80,10 +80,28 @@ function highestCount() {
   // seu código aqui
 }
 
+
+
+
+
+
+
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 > mouse && cat1 < cat2){
+    return "cat1";
+  }else if (cat2 > mouse && cat2 < cat1){
+    return "cat2";
+  }else if (cat1 == cat2 & cat1 == mouse && cat2 == mouse){
+    return "os gatos trombam e o rato foge";
+  }
 }
+
+
+
+
+
 
 
 
