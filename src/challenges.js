8c9622ce4e9517598eddd27f509dbe8d211e4c33
Thais Quintela <thais.s.quintela@gmail.com>
2020-10-29 19:05:00 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -67,11 +67,43 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(message) {
+  let newMessage = "";
+  for (let index in message) {
+    if (message[index] === "a") {
+      newMessage += "1";
+    } else if (message[index] === "e") {
+      newMessage += "2";
+    } else if (message[index] === "i") {
+      newMessage += "3";
+    } else if (message[index] === "o") {
+      newMessage += "4";
+    } else if (message[index] === "u") {
+      newMessage += "5";
+    } else {
+      newMessage += message[index];
+    }
+  }
+  return newMessage;
 }
-function decode() {
-  // seu código aqui
+function decode(message) {
+  let newMessage = "";
+  for (let index in message) {
+    if (message[index] === "1") {
+      newMessage += "a";
+    } else if (message[index] === "2") {
+      newMessage += "e";
+    } else if (message[index] === "3") {
+      newMessage += "i";
+    } else if (message[index] === "4") {
+      newMessage += "o";
+    } else if (message[index] === "5") {
+      newMessage += "u";
+    } else {
+      newMessage += message[index];
+    }
+  }
+  return newMessage;
 }
 
 // Desafio 10
