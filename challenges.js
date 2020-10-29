99a629496ae3743b1031189d84ced607d53f4cde
victor Farias <victorfarias.tv@gmail.com>
2020-10-29 13:27:40 -0300

    Arrumado requisito 1

--- challenges.js
+++ challenges.js
@@ -1,12 +1,11 @@
 // Usando o operador &&
 
 function compareTrue(bool1, bool2){
-  if ((bool1 === "true") && (bool2 === "true")) {
+  if (bool1 == true && bool2 == true) {
     return true;
   }
   else {
     return false;
   }
 };
-
-console.log(compareTrue("true", "true"));
+console.log(compareTrue(true, true));
