59f04343ee5627dd93daec210df2143defad497a
Samuel <samuelvanguarda@gmail.com>
2020-10-29 19:16:30 -0300

    Primeiro Requisito

---
+++ challenges.js
@@ -0,0 +1,25 @@
+//1 - Usando o operador &&
+
+function compareTrue(bool1,bool2){
+    let resultado;
+    if(bool1 === true && bool2 === true){
+        resultado = true; 
+    }else{
+        resultado = false;
+    }
+    console.log resultado;
+}
+compareTrue(true, true)
+
+
+
+//2 - Área do triângulo
+
+function calcArea(base, height)
+
+
+
+
+
+
+
