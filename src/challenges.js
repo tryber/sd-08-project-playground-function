59cbbbc35b2486340f41439503531cce0326fbc8
Rodolfo <rod.vgo@gmail.com>
2020-10-29 18:06:20 -0300

    dividindo string

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(stringToSplit) {
+  let spliWord = stringToSplit;
+    return spliWord.split(" ")
 }
 
 // Desafio 4
