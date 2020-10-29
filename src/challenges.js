4dd24c66db2d50bf46d4e3f672ae5e4952f51d03
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-10-29 19:48:47 -0300

    Corrige formatção do número de telefone

--- src/challenges.js
+++ src/challenges.js
@@ -159,11 +159,9 @@ function generatePhoneNumber(array) {
       }
     }
   }
-  let formatNumber = `(${array[0]}${array[1]}) ${array[2]}${array[2]}${array[3]}${array[4]}-${array[5]}${array[6]}${array[7]}${array[8]}`;
+  let formatNumber = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
   return formatNumber;
 }
-
-console.log(generatePhoneNumber([9, 2, 3, 0, 5, -6, 7, 8, -7, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
