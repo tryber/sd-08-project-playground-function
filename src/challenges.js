12735e5228dbbf7d7ad42eb8e7b1322b3caf41b8
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-29 15:33:53 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -48,8 +48,30 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1;
+  let distance2;
+  if(cat1 > mouse){
+      distance1 = cat1 - mouse;
+  }
+  else if(cat1 < mouse){
+      distance1 = mouse - cat1;
+  }
+  if(cat2 > mouse){
+      distance2 = cat2 - mouse;
+  }
+  else if(cat2 < mouse){
+      distance2 = mouse - cat2;
+  }
+  if(distance1 < distance2){
+      return "cat1"
+  }
+  else if(distance2 < distance1){
+      return "cat2"
+  }
+  else if(distance1 == distance2){
+      return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
--- src/rascunho.js
+++ src/rascunho.js
@@ -1,18 +1,29 @@
-function highestCount(numeros) {
-    let biggerNumber = 0
-    let repeat = 0
-    for(let index in numeros){
-        if(numeros[index] > biggerNumber){
-            biggerNumber = numeros[index]
-        }
-        else{}
+function catAndMouse(mouse, cat1, cat2) {
+    let distance1;
+    let distance2;
+    if(cat1 > mouse){
+        distance1 = cat1 - mouse;
     }
-    for(let i in numeros){
-        if(numeros[i] === biggerNumber){
-            repeat += 1
-        }
+    else if(cat1 < mouse){
+        distance1 = mouse - cat1;
     }
-    return repeat
+    if(cat2 > mouse){
+        distance2 = cat2 - mouse;
+    }
+    else if(cat2 < mouse){
+        distance2 = mouse - cat2;
+    }
+    if(distance1 < distance2){
+        return "cat1"
+    }
+    else if(distance2 < distance1){
+        return "cat2"
+    }
+    else if(distance1 == distance2){
+        return "os gatos trombam e o rato foge"
+    }
   }
 
-  console.log(highestCount([9, 9, 2, 3, 9, 5, 7]))
+  console.log(catAndMouse(5, 4, 1));
+  console.log(catAndMouse(1, 5, 2));
+  console.log(catAndMouse(4, 2, 6));
