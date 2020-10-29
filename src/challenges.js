381e3e0866bd37fc5f02c7071315b6158ed7de3b
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-29 15:36:02 -0300

    exercicio12 try1

--- src/challenges.js
+++ src/challenges.js
@@ -43,7 +43,17 @@ function catAndMouse() {
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  for (let i = 1; i <n; i ++){
+    if (i % 3 === 0 && i % 5 === 0){//Caso o número seja inteiro e multiplo de 3 e 5 imprime "FizzBuzz"
+        console.log("FizzBuzz") 
+    } else if (i % 3 === 0 && i % 5 !== 0){//Caso o número seja inteiro e multiplo apenas de 3 imprime "Fizz"
+        console.log("Fizz")
+    } else if (i % 3 !== 0 && i % 5 === 0){//Caso o número seja inteiro e multiplo apenas de 5 imprime "Buzz"
+        console.log("Buzz")
+    } else {
+        console.log(i) //Imprime os demais número que não sejam múltiplos de 3 ou 5
+    }
+}
 }
 
 // Desafio 9
@@ -59,15 +69,90 @@ function techList() {
   // seu código aqui
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {
+  let retorno="",retornoOk=[];
+  if(array.length == 11){
+    for(let i in array){
+
+      retorno.push(array[i])
+    }
+  } console.log(retorno)
+}generatePhoneNumber([1,2,3,4,5,6,7,8,9,10,11])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA,lineB,lineC) {
+  if(
+    lineA<(lineB+lineC) && 
+    lineB<(lineA+lineC) && 
+    lineC<(lineB+lineA) &&
+    lineA>Math.abs(lineB-lineC) && 
+    lineB>Math.abs(lineA-lineC) && 
+    lineC>Math.abs(lineB-lineA)){
+    return true;
+  } else{
+    return false;
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 13
 function hydrate() {
