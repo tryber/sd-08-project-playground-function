1c91957e3ac1734286c524b28e5b910798868eeb
Digo-Castro <dygo.castro@gmail.com>
2020-10-29 15:14:18 -0300

    Desafios 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,39 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a && b == true) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+//console.log (compareTrue(true, false))
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
+//console.log(calcArea(10, 0))
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  return phrase.split(' ');
 }
+//console.log(splitSentence("Rodrigo de Castro"))
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let first = array[0];
+  let last = array[array.length-1];
+  return last + ', ' + first;
 }
+//console.log(concatName(['Rodrigo', 'de Castro', 'Bandeira', 'Costa']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1)
 }
+console.log(footballPoints(3, 1));
 
 // Desafio 6
 function highestCount() {
