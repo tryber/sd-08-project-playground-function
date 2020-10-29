168b73bcf300692aa6b9d64e4de73d8fc2aa0b80
antonioarieiro <sfgfffs@gmail.com>
2020-10-29 20:05:56 -0300

    desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -38,12 +38,23 @@ function footballPoints(wins,ties) {
 //console.log(footballPoints(3,1))
 
 // Desafio 6
-function highestCount() {
+function highestCount(arrayNumbers) {
   // seu código aqui
+  let numbers = arrayNumbers.sort();
+  console.log (numbers)
+  let quantidadeDeVezesDoMaior = 0;
+  for (let a = 0; a < numbers.length; a += 1) {
+    if (numbers[numbers.length - 1] === numbers[a]) {
+      quantidadeDeVezesDoMaior += 1;
+      
+    }
+  }
+  return quantidadeDeVezesDoMaior;
 }
+//console.log(highestCount([1,9, 2, 3, 9, 5, 7, 9, 10,9]))
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
 }
 
