7fc5ea36dee64f345daa125d77bd90cbd4829cb8
Pedro Amaral <amaralph1995@gmail.com>
2020-10-29 14:54:18 -0300

    Adiciona os 6 primeiros exercícios do dia

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,44 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui hehe
+function compareTrue(a, b) {
+  return a && b;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(" ");
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let saida = array[array.length - 1] + ", " + array[0];
+  return saida;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  array.sort(function(a, b) {return a-b})
+  let cont = 0;
+
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    if (i == array.length - 1) {
+      cont += 1;
+    } else if (array[i] == array[i+1]) {
+      cont += 1;
+    } else {
+      break;
+    }
+  }
+  return cont;
 }
 
 // Desafio 7
