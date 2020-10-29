289fc747ade96d5d366e4c0a1c6514d7c84e464b
Blucilha <blucilha.nascimento@gmail.com>
2020-10-29 18:16:10 -0300

    Desafio 8 esta pronto

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
 function compareTrue(numero1, numero2) {
   if (numero1 && numero2 === true){
-     return true}
-     else {return false};
+  return true}
+  else {return false};
 }
 console.log(compareTrue(false, false));
 
@@ -15,7 +15,7 @@ console.log(calcArea(10, 4));
 
 // Desafio 3
 function splitSentence(string) { 
-  return string.split(" ");
+return string.split(" ");
  
 }
 console.log(splitSentence("Oi Tryber"))
@@ -23,7 +23,7 @@ console.log(splitSentence("Oi Tryber"))
 // Desafio 4
 function concatName(nome) {
   for (let index = 0; index < nome.length; index ++){
-      return nome[nome.length - 1] + "," + " " + nome[index];
+  return nome[nome.length - 1] + "," + " " + nome[index];
   }
 }
   console.log(concatName(["Bruce", "nascimento", "Pereira", "Zelia", "Dias"]));
@@ -51,17 +51,24 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let gato1 = Math.abs(cat1 - mouse);
-  let gato2 = Math.abs(cat2 - mouse);
-  if (gato1 < gato2){return `"cat1"`} else if (gato1 == gato2){return `"os gatos trombam e o rato foge"`}
+  if (cat1 < cat2){return `"cat1"`} else if (gato1 == gato2){return `"os gatos trombam e o rato foge"`}
   else {return `"cat2"`} 
 }
 console.log(catAndMouse(10, 7, 4))
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let recebeResposta = [];
+  for (let i = 0; i < array.length; i++){
+  if (array[i]%3 == 0 && array[i]%5 == 0){recebeResposta.push(`"fizzBuzz"`)}
+  else if (array[i]%3 == 0){recebeResposta.push(`"fizz"`)}
+  else if (array[i]%5 == 0){recebeResposta.push(`"buzz"`)}
+  else if (array[i]%3 !== 0 && array[i]%5 !== 0){recebeResposta.push(`"bug!"`)}
+
+  }
+  return recebeResposta;
 }
+console.log(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
 
 // Desafio 9
 function encode() {
