e4972616a5bd089a037a55284b47f9ee9af97602
Victor Breno <victor.brns2@gmail.com>
2020-10-29 15:28:58 -0300

    resolução do setimo desafio

--- src/challenges.js
+++ src/challenges.js
@@ -45,8 +45,16 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(mouse-cat1);
+  let distanciaCat2 = Math.abs(mouse-cat2);
+  if (distanciaCat1 > distanciaCat2) {
+    return("cat2");
+  } else if (distanciaCat1 < distanciaCat2) {
+    return("cat1");
+  } else {
+    return("os gatos trombam e o rato foge");
+  }
 }
 
 // Desafio 8
