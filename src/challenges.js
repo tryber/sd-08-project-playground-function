6275cd0de1d14c5a80feb0521c46ccb1773ee32c
Silvio Dayube <scarige@gmail.com>
2020-10-29 14:59:48 -0300

    Desafio 5 - Pontos no Futebol

--- src/challenges.js
+++ src/challenges.js
@@ -30,14 +30,15 @@ function splitSentence(str) {
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ', ' + array[0];
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
+console.log(footballPoints(15, 6));
 // Desafio 6
 function highestCount() {
   // seu código aqui
