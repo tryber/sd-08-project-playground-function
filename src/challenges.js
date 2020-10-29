e948b3efc975847a3c4cb292d8fd79204fa392ec
Daniel Leon <daniel.radialista@gmail.com>
2020-10-29 19:02:28 -0300

    exercicio 5

--- src/challenges.js
+++ src/challenges.js
@@ -36,9 +36,10 @@ function concatName(strings) {
   
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  return((wins*3) + ties);
 }
+
 
 // Desafio 6
 function highestCount() {
