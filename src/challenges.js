2ff5011cab740efb28aeb785687a148018ec455b
enicio <jessandro42@gmail.com>
2020-10-29 17:21:44 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -65,10 +65,30 @@ function catAndMouse(mouse, cat1, cat2) {
 
 console.log(catAndMouse(1, 0, 2))
 
+
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayToReturn = [];
+
+  for ( let i = 0; i < array.length; i += 1 ) {
+
+    if (array[i] % 3 == 0 && array[i] % 5 == 0) {
+      arrayToReturn[i] = "fizzBuzz"
+    }else if ((array[i] % 3) == 0 ) {
+      arrayToReturn[i] = "fizz"
+    } else if ( array[i] % 5 == 0) {
+      arrayToReturn[i] = "buzz"
+    } else {
+      arrayToReturn[i] = "bug!"
+    }
+  }
+  return arrayToReturn
 }
+
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
+
+
 
 // Desafio 9
 function encode() {
