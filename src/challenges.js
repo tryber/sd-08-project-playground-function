e2f5027a1d32b8e23addce87bbc169ecd71009cb
Hugo Andrade Braga <hugobrr@gmail.com>
2020-10-29 17:02:48 -0300

    vamos começar a revisasr os codigos

--- src/challenges.js
+++ src/challenges.js
@@ -107,7 +107,7 @@ for (const key in array){
     resultado[key] += "Buzz";
   }
   else{
-    resultado[key] = "bug!"
+    resultado[key] = "bug!";
   }
 }
 }
@@ -152,7 +152,8 @@ function techList() {
 }
 // Desafio 11
 function generatePhoneNumber() {
-//   Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como parâmetro e retornar se é possível formar um triângulo com os valores apresentados de cada linha
+//   Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como
+//parâmetro e retornar se é possível formar um triângulo com os valores apresentados de cada linha
 
 // Para tanto, tenha em mente algumas considerações:
 
@@ -166,18 +167,22 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-//   Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como parâmetro e retornar se é possível formar um triângulo com os valores apresentados de cada linha
+function triangleCheck(lineA,lineB,lineC) {
+//   Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como parâmetro e
+//retornar se é possível formar um triângulo com os valores apresentados de cada linha
 
 // Para tanto, tenha em mente algumas considerações:
 
-// Para que seja possível formar um triângulo, é necessário que a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas.
+// Para que seja possível formar um triângulo, é necessário que a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior
+// que o valor absoluto da diferença entre essas medidas.
 
 // Para obter o valor absoluto de um número em JavaScript, pesquise pela função Math.abs.
 
 // O retorno da sua função deverá ser um booleano.
 
 // Exemplo: o retorno de triangleCheck(10, 14, 8) deverá ser true.
+
+
 }
 
 // Desafio 13
