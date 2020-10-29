20221ebbd7877b2f36e555677d7275ba1ec0ffe4
rafasysop <rafaelcostamoura@gmail.com>
2020-10-29 12:16:45 -0300

    enviando desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -28,8 +28,9 @@ function concatName(param1) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins,ties) {
   // seu código aqui
+  return wins*3 + ties;
 }
 
 // Desafio 6
