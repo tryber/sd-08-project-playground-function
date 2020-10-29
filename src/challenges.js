d2e57b23efc362f9a7a0bc92c5c67a84cc357b46
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-10-29 17:19:26 -0300

    adding exercises 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,22 @@
-//Desafio 1
-let value1 = true;
-let value2 = true;
-function compareTrue(value1, value2) {
+// Desafio 1
+function compareTrue(value1,value2) {
+
     if(value1 && value2 === true) {
         return true;
         } else {
-        return false
+        return false;
         }
-
-        console.log(compareTrue(value1, value2));
-
-// Desafio 2
-
-function calcArea(base, height) {
-  return (base * height) / 2;
 }
 
-console.log(calcArea(10, 5));
+// Desafio 2
+function calcArea(base,heigh) {
+    return(base*height)/2;
+}
 
 // Desafio 3
-function splitSentence() {
-  
+function splitSentence(word) {
+  let resultado = word.split("");
+  return resultado;
 }
 
 // Desafio 4
@@ -41,4 +37,55 @@ function highestCount() {
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
+}
+
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
+
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
 }
