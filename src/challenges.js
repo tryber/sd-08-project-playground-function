9e763dd0b6e899dcb03c0871afe50389fa6ed838
Vinicius <viniciussilvaribeiro@gmail.com>
2020-10-29 20:54:55 -0300

    tentando subir

--- src/challenges.js
+++ src/challenges.js
@@ -36,13 +36,46 @@ splitSentence("Go Trybe");
 
 
 
+// Desafio 4
+function concatName(array) {
+  // seu código aqui
+  let lista = array[array.length -1] + ', ' + array[0];
+  return lista;
+}
+concatName(['lucas', 'cassiano', 'ferraz', 'Paolillo']);
 
+
+
+
+
+// Desafio 5
+function footballPoints(wins, ties) {
+  // seu código aqui
+  return (wins * 3) - ties;
+}
+
+footballPoints(3, 2);
 
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
+  let maior = array[0];
+  let repet = 0;
+  for (let cont = 1; cont < array.length; cont++) {
+    if (array[cont] > maior) {
+      maior = array[cont];
+    }
+  } 
+  for (let cont = 0; cont < array.length; cont++) {
+    if (maior === array[cont]) {
+      repet += 1;
+    }
+  }
+  return repet;
 }
+highestCount([3, 1, 2, 3, 9, 5, 7]);
+
 
 
 
@@ -54,14 +87,37 @@ function catAndMouse() {
 
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(divisivel) {
   // seu código aqui
-}
+  let array = [];
+    for (let cont = 0; cont <= divisivel.length; cont++) {
+      if (divisivel[cont] % 3 === 0 && divisivel[cont] % 5 === 0) {
+        array.push("fizzbuzz");
+      } else if (divisivel[cont] % 5 === 0) {
+        array.push("buzz");
+      } else if (divisivel[cont] % 3 === 0) {
+        array.push("fizz");
+      } else {
+        array.push("bug");
+      }
+    }
+    return array;
+    }
+    console.log(fizzBuzz([2, 3, 5, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
+function encode(vogais) {
   // seu código aqui
+  vogais = vogais.replace(/a/g , "1");
+  vogais = vogais.replace(/e/g , "2");
+  vogais = vogais.replace(/i/g , "3");
+  vogais = vogais.replace(/o/g , "4");
+  vogais = vogais.replace(/u/g , "5");
+  return vogais;
 }
+encode("naaaaeiou");
+
+
 function decode() {
   // seu código aqui
 }
