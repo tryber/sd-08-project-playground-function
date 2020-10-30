c972d25bbb88386a792c900a72a125384d8200e3
Bruno Martins <bruno.martins217@gmail.com>
2020-10-29 22:45:28 -0300

    Terceira tentativa Desafio 10

--- src/challenges.js
+++ src/challenges.js
@@ -138,8 +138,8 @@ function decode(string) {
 }//console.log(decode(str2))
 
 // Desafio 10
-//let arrMat = ["React", "Jest", "HTML", "CSS", "JavaScript"]
-//let strName = "Brn"
+let arrMat = []
+let strName = "Brn"
 
 //let obj = {}
 
@@ -148,7 +148,7 @@ function techList(array, name) {
   let materias = array.sort()
   let error = "Vazio!"
   let arrayObj = []
-  if(nome.length === 0){
+  if(materias.length === 0){
     return error
   }else {
     for (i in materias){
@@ -160,7 +160,7 @@ function techList(array, name) {
     }
   }
   return arrayObj
-}//console.log(techList(arrMat, strName))
+}console.log(techList(arrMat, strName))
 
 // Desafio 11
 function generatePhoneNumber() {
