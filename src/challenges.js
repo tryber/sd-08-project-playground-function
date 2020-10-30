22a7714fc3a1ebace2268e5120d2f2aaa41bf82e
Vinicius <viniciussilvaribeiro@gmail.com>
2020-10-29 21:42:27 -0300

    corrigindo a 8

--- src/challenges.js
+++ src/challenges.js
@@ -103,7 +103,7 @@ function fizzBuzz(divisivel) {
     }
     return array;
     }
-    console.log(fizzBuzz([2, 15, 7, 9, 45]));
+    fizzBuzz([2, 15, 7, 9, 45]);
 
 // Desafio 9
 function encode(vogais) {
