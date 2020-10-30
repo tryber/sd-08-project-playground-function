477d6a5e7849a664b5d8fbf8e3e4886d170a05c0
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-10-30 18:04:30 -0300

    correção requisito7-8

--- src/challenges.js
+++ src/challenges.js
@@ -28,17 +28,17 @@ function splitSentence(frase) {
 console.log(splitSentence(frase));
 
 // Desafio 4
-let array = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo', 'Ana', 'Luiz Fernando', 'José']
+let array = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
 function concatName(array) {
   // seu código aqui
   for (let i = 0; i < array.length -1; i += 1) {
-    return (array[array.length-1] + ','  + array[0])
+    return (array[array.length-1] + ', '  + array[0])
   }
 }
 console.log(concatName(array));
 
 // Desafio 5
-let wins = 0;
+let wins = 12;
 let ties = 9;
 function footballPoints(wins, ties) {
   // seu código aqui
@@ -74,7 +74,7 @@ function catAndMouse(mouse, cat1, cat2) {
   let distMouseCat2 = Math.abs(mouse - cat2);
   let sameDistance = Math.abs(cat1 - cat2);
   if (sameDistance == 0) {
-    return 'os gatos trombam e o rato foge.'
+    return 'os gatos trombam e o rato foge'
   } else if (distMouseCat1 < distMouseCat2) {
     return 'cat1'
   } else {
@@ -90,7 +90,7 @@ function fizzBuzz(parametro) {
   let array = [];
   for (let cont = 0; cont < parametro.length; cont += 1) {
     if (parametro[cont] % 15 == 0) {
-      array[cont] = 'fizzbuzz'
+      array[cont] = 'fizzBuzz'
     } else if (parametro[cont] % 3 == 0) {
        array[cont] = 'fizz'
     } else if (parametro[cont] % 5 == 0) {
@@ -105,9 +105,29 @@ function fizzBuzz(parametro) {
 console.log(fizzBuzz(parametro));
 
 // Desafio 9
-function encode() {
+let saudacao = 'hi there!"';
+
+function encode(saudacao){
   // seu código aqui
+  let codificada = '';
+  let vogais = {
+    a: 1,
+    e:2,
+    i:3,
+    o:4,
+    u:5,
+  }
+  for (let letra of saudacao) { // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/for...of
+    if (vogais[letra] === undefined) { //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/undefined
+      codificada += letra
+    } else {
+      codificada += vogais[letra]
+    }
+  }
+  return codificada
 }
+console.log(encode(saudacao));
+
 function decode() {
   // seu código aqui
 }
