8c458ff83c62408e18817b09404b060b25e260f7
Uilton <itoclick@gmail.com>
2020-10-30 16:32:35 -0300

    removento consolelog

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,16 @@
 // Desafio 1
-function compareTrue(a,b) {
-
-  if (a===true && b===true){
+function compareTrue() {
+  
+  if (a==true && b==true){
     return "true";
   } else {return "false";
   };
-  };
-  console.log (compareTrue(true,true));
+  
+
+};
+
+
+  
   
   // seu código aqui
 
