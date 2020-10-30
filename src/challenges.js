bec5502a02fa050a610affd4e504a554a49b09e9
Claudio <devtrybe@gmail.com>
2020-10-30 14:08:05 -0300

    desafio 7 - correção aspas das strings

--- src/challenges.js
+++ src/challenges.js
@@ -43,7 +43,7 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   if ((Math.abs(cat1-mouse)) < (Math.abs(cat2-mouse))) {
     return "cat1";
-  } else if ((Math.abs(cat1-mouse)) < (Math.abs(cat2-mouse))) {
+  } else if ((Math.abs(cat1-mouse)) > (Math.abs(cat2-mouse))) {
     return "cat2";
   } else {
     return "os gatos trombam e o rato foge";
