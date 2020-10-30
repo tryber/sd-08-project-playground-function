0e71e027a4a14809b2847e293332e2da2e1a1153
Bidfca <bruninhakjurugat11@gmail.com>
2020-10-29 22:09:39 -0300

    Initial commit

--- src/challenges.js
+++ src/challenges.js
@@ -1,41 +1,70 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 && value2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(" ");
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arr) {
+  return arr.slice(0, 1) + " " + arr.slice(-1);
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let total = 0;
+  while (wins > 0) {
+    total += 3;
+    wins--;
+  }
+  while (ties > 0) {
+    total += 1;
+    ties--;
+  }
+  return total;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  let largest = 0;
+  let number = 0;
+  arr.forEach(value => {
+    if (value > largest) largest = value;
+  });
+  arr.forEach(value => value == largest && number++ );
+  return number;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  return Math.abs(cat1 - mouse) == Math.abs(cat2 - mouse)? "os gatos trombam e o rato foge": Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)? "cat2": "cat1";
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  arr.forEach(function (value) {
+    if(value % 3 == 0 && value % 5 == 0) {
+      console.log("fizzBuzz");
+    } else if(value % 3 == 0) {
+      console.log("fizz");
+    } else if(value % 5 == 0) {
+      console.log("buzz");
+    } else {
+      console.log("bug!");
+    }
+  });
 }
 
 // Desafio 9
---
+++ src/teste.js
@@ -0,0 +1,9 @@
+const Teste = require('./challenges.js');
+console.log(Teste.compareTrue(1,2));
+console.log(Teste.calcArea(1,4));
+console.log(Teste.splitSentence("bianca caetano"));
+console.log(Teste.concatName(["bianca", "de", "oliveira", "caetano"]));
+console.log(Teste.footballPoints(3, 2));
+console.log(Teste.highestCount([2, 5, 3, 4, 4, 4, 5, 6]));
+console.log(Teste.catAndMouse(2, 5, 3));
+console.log(Teste.fizzBuzz([2, 3, 4, 5, 10, 11]));
