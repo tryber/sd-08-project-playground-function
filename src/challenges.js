93614c1c0a95d9e056ad8633eb00571bca25167c
Nícolas Tozato <ntozato9@gmail.com>
2020-10-30 16:07:56 -0300

    requisito 9, nova tentativa

--- src/challenges.js
+++ src/challenges.js
@@ -107,19 +107,19 @@ function encode(string) {
   for (let i = 0; i < string.length; i++) {
 
     if (string[i] == 'a') {
-      resultado = string.replace('a', 1);
+      resultado = string.replace(/a/g, 1);
 
     } else if (string[i] == 'e') {
-      resultado = string.replace('e', 2);
+      resultado = string.replace(/e/g, 2);
 
     } else if (string[i] == 'i') {
-      resultado = string.replace('i', 3);
+      resultado = string.replace(/i/g, 3);
 
     } else if (string[i] == 'o') {
-      resultado = string.replace('o', 4);
+      resultado = string.replace(/o/g, 4);
 
     } else if (string[i] == 'u') {
-      resultado = string.replace('u', 5);
+      resultado = string.replace(/u/g, 5);
     }
   }
 
@@ -136,19 +136,19 @@ function decode(string2) {
   for (let i2 = 0; i2 < string2.length; i2++) {
 
     if (string2[i2] == 1) {
-      resultado2 = string2.replace(1, 'a');
+      resultado2 = string2.replace(/1/g, 'a');
 
     } else if (string2[i2] == 2) {
-      resultado2 = string2.replace(2, 'e');
+      resultado2 = string2.replace(/2/g, 'e');
 
     } else if (string2[i2] == 3) {
-      resultado2 = string2.replace(3, 'i');
+      resultado2 = string2.replace(/3/g, 'i');
 
     } else if (string2[i2] == 4) {
-      resultado2 = string2.replace(4, 'o');
+      resultado2 = string2.replace(/4/g, 'o');
 
     } else if (string2[i2] == 5) {
-      resultado2 = string2.replace(5, 'u');
+      resultado2 = string2.replace(/5/g, 'u');
 
     }
   }
