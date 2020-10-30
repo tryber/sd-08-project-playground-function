9328b8a8dd6245249b85681a96b69d854f7929df
Victor Moraes <moraesvictorgk@gmail.com>
2020-10-30 15:38:20 -0300

    gato e rato

--- src/challenges.js
+++ src/challenges.js
@@ -26,7 +26,8 @@ function concatName(lista) {
   let ordemInversa = []
   for (var index = lista.length -1;index>=0;index-=1){
     ordemInversa.push (lista[index]);
-      } return ordemInversa;
+    } 
+      return ordemInversa;
  }
  
 // Desafio 5
@@ -41,9 +42,21 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse,cat1,cat2) {
+  cat1=cat1-mouse;
+  cat2=cat2-mouse;
+  if (Math.abs(cat1) < Math.abs(cat2)){
+    return "cat1";
+  }
+  
+  else if (Math.abs(cat2) < Math.abs(cat1)) {
+      return "cat2"
+    }
+  else if (Math.abs(cat2) == Math.abs(cat1)){
+    console.log("os os gatos trombam e o rato foge")
+  }
+  }
+
 
 // Desafio 8
 function fizzBuzz(array) {
