e100acbfde32382350ea9d381d2d3156a404e475
Digo-Castro <dygo.castro@gmail.com>
2020-10-30 20:34:31 -0300

    Ajuste Desafio 9 e Desafio 10

--- src/challenges.js
+++ src/challenges.js
@@ -100,121 +100,81 @@ function fizzBuzz(arrayNum) {
 // console.log(fizzBuzz([3, 1, 5, 10, 12, 14, 15, 19, 20]))
 
 // Desafio 9
-function funcArray1(array) {
-  let key;
-  let array1 = [];
-  for (key in array) {
-    if (array[key] === 'a') {
-      array1.push('1');
-    } else if (array[key] === '1') {
-      array1.push('a');
-    } else {
-      array1.push(array[key]);
-    }
-  }
-  return array1;
-}
-
-function funcArray2(array) {
-  let key;
-  let array2 = [];
-  for (key in array) {
-    if (array[key] === 'e') {
-      array2.push('2');
-    } else if (array[key] === '2') {
-      array2.push('e');
-    } else {
-      array2.push(array[key]);
-    }
-  }
-  return array2;
-}
-
-function funcArray3(array) {
-  let key;
-  let array3 = [];
-  for (key in array) {
-    if (array[key] === 'i') {
-      array3.push('3');
-    } else if (array[key] === '3') {
-      array3.push('i');
-    } else {
-      array3.push(array[key]);
-    }
-  }
-  return array3;
-}
-
-function funcArray4(array) {
-  let key;
-  let array4 = [];
-  for (key in array) {
-    if (array[key] === 'o') {
-      array4.push('4');
-    } else if (array[key] === '4') {
-      array4.push('o');
-    } else {
-      array4.push(array[key]);
-    }
-  }
-  return array4;
-}
-
-function funcArray5(array) {
-  let key;
-  let array5 = [];
-  for (key in array) {
-    if (array[key] === 'u') {
-      array5.push('5');
-    } else if (array[key] === '5') {
-      array5.push('u');
-    } else {
-      array5.push(array[key]);
-    }
-  }
-  return array5;
-}
-
 function translater(phrase) {
   let arrayChar = phrase.split('');
-  let array1 = funcArray1(arrayChar);
-  let array2 = funcArray2(array1);
-  let array3 = funcArray3(array2);
-  let array4 = funcArray4(array3);
-  let array5 = funcArray5(array4);
-  let newPhrase = array5.join('');
-  return newPhrase;
+  let key;
+  novoArr = [];
+  for (key in arrayChar){
+    switch (arrayChar[key]) {
+      case 'a':
+        novoArr.push('1');
+        break;
+      case 'e':
+        novoArr.push('2');
+        break;
+      case 'i':
+        novoArr.push('3');
+        break;
+      case 'o':
+        novoArr.push('4');
+        break;
+      case 'u':
+        novoArr.push('5');
+        break;
+      case '1':
+        novoArr.push('a');
+        break;
+      case '2':
+        novoArr.push('e');
+        break;
+      case '3':
+        novoArr.push('i');
+        break;
+      case '4':
+        novoArr.push('o');
+        break;
+      case '5':
+        novoArr.push('u');
+        break;
+      default:
+        novoArr.push(arrayChar[key]);
+    }
+  }
+  return novoArr.join('');
 }
 
 function encode(phrase) {
   return translater(phrase);
 }
-// console.log(encode("observe que essa mensagem ficou oculta"));
+ console.log(encode("observe que essa mensagem ficou oculta"));
 
 function decode(phrase) {
   return translater(phrase);
 }
-// console.log(decode('4bs2rv2 q52 2ss1 m2ns1g2m n14 f3c45 4c5lt1 12345'));
+ console.log(decode('4bs2rv2 q52 2ss1 m2ns1g2m n14 f3c45 4c5lt1 12345'));
 
 // Desafio 10
 function makeObject(tech, name) {
-  let objTech = new Object();
-  objTech.tech = tech;
-  objTech.name = name;
+  let objTech = {
+  tech: tech,
+  name: name,
+  };
   return objTech;
 }
 
 function techList(array, name) {
+  let res;
   if (array.length !== 0) {
     let key;
     let newArray = [];
     for (key in array) {
     newArray.push(makeObject(array[key], name));
     }
-    return newArray;
-  } else {  
-    return "Vazio!";
+    res = newArray;
+  } else {
+    res = 'Vazio!';
   }
+  return res;
 }
 // console.log(techList([], 'Digo'))
 
