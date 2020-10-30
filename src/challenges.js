201e565bda2066c23fd5d5468bb7725f96fb7c3f
Claudio <devtrybe@gmail.com>
2020-10-30 11:12:18 -0300

    desafio 6 refatorado

--- src/challenges.js
+++ src/challenges.js
@@ -27,12 +27,12 @@ function footballPoints(wins, ties) {
 function highestCount(array) {
   let maior = array[0];
   let counter = 0;
-  for (let pos = 0; pos < array.length; pos += 1) {
+  for (pos in array) {
     if (array[pos] > maior) {
       maior = array[pos];
     }
   }
-  for (let pos = 0; pos < array.length; pos += 1) {
+  for (pos in array) {
     if (array[pos] === maior) {
       counter += 1;
     }
