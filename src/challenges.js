95389532cf42ffe6af6f11b3740b3669ad6d0037
wagnerberna <cabelo29007@gmail.com>
2020-10-31 07:24:23 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -105,9 +105,29 @@ fizzBuzz(testeFizzBuzz);
 //console.log(fizzBuzz(vetorTeste));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
+  function encode(string) {
+    let = resultado = "";
+    resultado = string.replace(/a/g, "1");
+    resultado = resultado.replace(/e/g, "2");
+    resultado = resultado.replace(/i/g, "3");
+    resultado = resultado.replace(/o/g, "4");
+    resultado = resultado.replace(/u/g, "5");
+    return resultado; 
+  }
+  function decode(string) {
+    let = resultado = "";
+    resultado = string.replace(/1/g, "a");
+    resultado = resultado.replace(/2/g, "e");
+    resultado = resultado.replace(/3/g, "i");
+    resultado = resultado.replace(/4/g, "o");
+    resultado = resultado.replace(/5/g, "u");
+    return resultado;
+  }
+  let testeEncode = "hi there!"
+  let testeDecode = "h3 th2r2!"
+  encode(testeEncode);
+  decode(testeDecode);
+  //console.log(encode(teste));
 function decode() {
   // seu código aqui
 }
