bcd7cd0431309f0f151efae96db4a928a707d363
Ruben Albertassi <albertassi88@gmail.com>
2020-10-30 22:28:38 -0300

    Alteracao no exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -66,9 +66,9 @@ function highestCount(arrayNumeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs(cat1 > mouse) && Math.abs(cat1 < cat2)){
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
     return "cat1";
-  }else if (Math.abs(cat2 > mouse) && Math.abs(cat2 < cat1)){
+  }else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
     return "cat2";
   }else {
     return "os gatos trombam e o rato foge";
