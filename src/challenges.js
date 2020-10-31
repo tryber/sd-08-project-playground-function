14c39c5c705c8e1a20803fb10229c7e544a3fc24
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-10-31 01:51:42 -0300

    correcao 2 - code climate

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,11 @@
 // Desafio 1
 function compareTrue(status1, status2) {
   return status1 && status2;
-  }
+}
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height)/2;
+  let area = (base * height) / 2;
   return area;
 }
 
@@ -17,7 +17,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(arrayStrig) {
-  return `${arrayStrig[arrayStrig.length-1]}, ${arrayStrig[0]}`
+  return `${arrayStrig[arrayStrig.length - 1]}, ${arrayStrig[0]}`
 }
 
 // Desafio 5
@@ -29,14 +29,14 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   let max = 0;
-  for(let fixed = 0; fixed < array.length; fixed += 1) {
-    for(let find = fixed + 1; find < array.length; find += 1) {
-      if(array[fixed] < array[find]) {
+  for (let fixed = 0; fixed < array.length; fixed += 1) {
+    for (let find = fixed + 1; find < array.length; find += 1) {
+      if (array[fixed] < array[find]) {
         max = array[find];
-        fixed = find-1;
+        fixed = find - 1;
         find = array.length;
       }
-      else if (find == array.length-1) {
+      else if (find == array.length - 1) {
         max = array[fixed];
         fixed = array.length;
       }
@@ -44,8 +44,8 @@ function highestCount(array) {
   }
   let pos = array.indexOf(max);
   let count = 0;
-  for(let index = pos; index < array.length; index += 1) {
-    if(max == array[index]) {
+  for (let index = pos; index < array.length; index += 1) {
+    if (max == array[index]) {
       count += 1;
     }
   }
@@ -56,40 +56,38 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = 0;
   let distanciaCat2  = 0;
-  if(cat1 > mouse) {
+  if (cat1 > mouse) {
     distanciaCat1 = cat1 - mouse;
   }
   else {
     distanciaCat1 = mouse - cat1;
   }
-  if(cat2 > mouse) {
+  if (cat2 > mouse) {
     distanciaCat2 = cat2 - mouse;
   }
   else {
     distanciaCat2 = mouse - cat2;
   }
-  if(distanciaCat1 < distanciaCat2) {
+  if (distanciaCat1 < distanciaCat2) {
     return 'cat1';
   }
-  else if(distanciaCat2 < distanciaCat1) {
+  else if (distanciaCat2 < distanciaCat1) {
     return 'cat2';
   }
-  else {
-    return 'os gatos trombam e o rato foge'
-  }
+  return 'os gatos trombam e o rato foge'
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let newArray = [];
-  for(let index in array) {
-    if(array[index]%3 == 0 && array[index]%5 == 0) {
+  for (let index in array) {
+    if (array[index] % 3 == 0 && array[index] % 5 == 0) {
       newArray.push('fizzBuzz');
     }
-    else if(array[index]%3 == 0) {
+    else if (array[index] % 3 == 0) {
       newArray.push('fizz');
     }
-    else if(array[index]%5 == 0) {
+    else if (array[index] % 5 == 0) {
       newArray.push('buzz');
     }
     else {
@@ -110,9 +108,9 @@ function encode(text) { //hi there!
   };
   let arrayString = text.split('');
   let newText = '';
-  for(let index = 0; index < text.length; index += 1) {
-    for(let key in codec) {
-      if(key == arrayString[index]) {
+  for (let index = 0; index < text.length; index += 1) {
+    for (let key in codec) {
+      if (key == arrayString[index]) {
         arrayString[index] = codec[key];
       }
     }
@@ -131,9 +129,9 @@ function decode(text) {
   };
   let arrayString = text.split('');
   let newText = '';
-  for(let index = 0; index < text.length; index += 1) {
-    for(let key in codec) {
-      if(codec[key] == arrayString[index]) {
+  for (let index = 0; index < text.length; index += 1) {
+    for (let key in codec) {
+      if (codec[key] == arrayString[index]) {
         arrayString[index] = key;
       }
     }
@@ -144,12 +142,12 @@ function decode(text) {
 
 // Desafio 10
 function techList(arrayTec, name) {
-  if(arrayTec.length == 0 || arrayTec == null) {
+  if (arrayTec.length == 0 || arrayTec == null) {
     return 'Vazio!';
   }
   newList = [];
   let arraySort = arrayTec.sort();
-  for(let index in arraySort) {
+  for (let index in arraySort) {
     let object = {
       tech: '',
       name: ''
@@ -164,31 +162,31 @@ function techList(arrayTec, name) {
 // Desafio 11
 function generatePhoneNumber(arrayPhone) {
   let phoneNumber;
-  if(arrayPhone.length != 11) {
+  if (arrayPhone.length != 11) {
     return 'Array com tamanho incorreto.'
   }
-  for(let index in arrayPhone) {
+  for (let index in arrayPhone) {
     let contEvent = 0;
-    for(let i = 0; i < arrayPhone.length; i += 1) {
-      if(arrayPhone[index] == arrayPhone[i]) {
+    for (let i = 0; i < arrayPhone.length; i += 1) {
+      if (arrayPhone[index] == arrayPhone[i]) {
         contEvent += 1;
       }
     }
-    if(arrayPhone[index] < 0 || arrayPhone[index] > 9 || contEvent >= 3) {
+    if (arrayPhone[index] < 0 || arrayPhone[index] > 9 || contEvent >= 3) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
-    if(index == 0) {
+    if (index == 0) {
       phoneNumber = '(' + arrayPhone[index];
     }
-    else if(index == 1) {
+    else if (index == 1) {
       phoneNumber += arrayPhone[index] + ') ';
     }
-    else if(index == 6) {
+    else if (index == 6) {
       phoneNumber += arrayPhone[index] + '-';
     }
     else {
       phoneNumber += arrayPhone[index];
-    }   
+    }
   }
   return phoneNumber;
 }
@@ -201,12 +199,10 @@ function triangleCheck(lineA, lineB, lineC) {
   let condicao4 = Math.abs(lineA - lineB) < lineC;
   let condicao5 = Math.abs(lineA - lineC) < lineB;
   let condicao6 = Math.abs(lineB - lineC) < lineA;
-  if(condicao1 && condicao2 && condicao3 && condicao4 && condicao5 && condicao6) {
+  if (condicao1 && condicao2 && condicao3 && condicao4 && condicao5 && condicao6) {
     return true
   }
-  else {
-    return false
-  }
+  return false
 }
 
 // Desafio 13
@@ -214,15 +210,13 @@ function hydrate(string) {
   let regex = /\d+/g;
   let cupsWater = 0;
   let numbers = string.match(regex);
-  for(let index in numbers) {
+  for (let index in numbers) {
     cupsWater += parseInt(numbers[index]);
   }
-  if(cupsWater == 1) {
+  if (cupsWater == 1) {
     return `${cupsWater} copo de água`;
   }
-  else {
-    return `${cupsWater} copos de água`;
-  }
+  return `${cupsWater} copos de água`;
 }
 
 module.exports = {
