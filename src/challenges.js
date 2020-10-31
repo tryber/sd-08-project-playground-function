412c1e97e69c34c1786d6923fd7128f0c99b7934
victor718 <victorfelipeoliveira1@gmail.com>
2020-10-31 20:14:32 -0300

    corrigido problema desafio 13 parseInt

--- src/challenges.js
+++ src/challenges.js
@@ -254,12 +254,12 @@ function hydrate(drinks) {
   let total = 0;
 
   for (let x = 0; x < resultado.length; x += 1) {
-    total += parseInt(resultado[x], 2);
+    total += parseInt(resultado[x], 0);
   }
 
   if (total === 1) {
     return `${total} copo de água`;
-  } 
+  }
   return `${total} copos de água`;
 }
 module.exports = {
