2cbce89c296b17c7fc37d489b048c6d34020088b
Blucilha <blucilha.nascimento@gmail.com>
2020-10-31 14:35:14 -0300

    Desafio 7 de novo

--- src/challenges.js
+++ src/challenges.js
@@ -53,8 +53,8 @@ function highestCount(numbers) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)){return "cat1"} 
-  else if (cat1 === cat2){return "os gatos trombam e o rato foge"}
-  else {return "cat2"}
+  else if (Math.abs(mouse - cat2) < Math.abs(mouse - cat1)){return "cat2"}
+  else {return "os gatos trombam e o rato foge"}
 }
 
 
