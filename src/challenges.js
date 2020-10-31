bf17758289c416929a38acc265ac71d29e5c4534
Erick Santos <erickgaves@gmail.com>
2020-10-31 18:02:27 -0300

    corrigindo desafios 09 e 10

--- src/challenges.js
+++ src/challenges.js
@@ -72,14 +72,14 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   let stringSwap = { "a": "1", "e": "2", "i": "3", "o": "4", "u": "5" };
   let encondeResult = "";
   encondeResult = string.replace(/[aeiou]/gi, m => stringSwap[m]);
 
   return encondeResult;
 }
-function decode() {
+function decode(string) {
   let stringSwap = { "1": "a", "2": "e", "3": "i", "4": "o", "5": "u" }
   let encondeResult = "";
   encondeResult = string.replace(/[12345]/gi, m => stringSwap[m]);
@@ -88,7 +88,7 @@ function decode() {
 }
 
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   let resultArray = [];
   let orderedTechList = tech.sort();
   if (tech.length === 0) {
