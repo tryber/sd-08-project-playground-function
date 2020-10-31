6d05821f3f9a21dcf661a6b25dc88df9728c6a72
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-10-30 23:58:59 -0300

    Codeclimate spacings and infix operators

--- src/challenges.js
+++ src/challenges.js
@@ -1,83 +1,91 @@
 // Desafio 1
-function compareTrue(value1,value2) {
-  return value1 &&value2;
+function compareTrue(value1 , value2) {
+  return value1 && value2;
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  let area = (base*height)/2;
+function calcArea(base , height) {
+  let area = (base * height)/2;
   return area;
 }
 
 // Desafio 3
 function splitSentence(string) {
-    let list = string.split(" ");
-    return list;
+  let list = string.split(' ');
+  return list;
 }
 
 // Desafio 4
 function concatName(array) {
-  let arrayFinal= [];
-  arrayFinal.push(array[array.length-1]);
+  let arrayFinal = [];
+  arrayFinal.push(array[array.length - 1]);
   arrayFinal.push(array[0]);
-  return arrayFinal.join(", ");
+  return arrayFinal.join(', ');
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = wins * 3 + ties;
+  let points = (wins * 3) + ties;
   return (points);
 }
 
 // Desafio 6
-function getHighestNumber(array){
-  let maxNumber = array.reduce(function(a,b) {
-    return Math.max(a,b);
+function getHighestNumber(array) {
+  let maxNumber = array.reduce(function(a , b) {
+  return Math.max(a , b);
   });
   return maxNumber;
-};
-function highestCount(array) {
-let number = getHighestNumber(array);
-let freq = 0;
-for (i in array) {
-  (array[i] == number)? freq += 1: freq = freq;
 }
-return freq;
+function highestCount(array) {
+  let number = getHighestNumber(array);
+  let freq = 0;
+  for (let i = 0; i<= array.length; i += 1) {
+    if(array[i] === number) { 
+      freq += 1;
+    }
+  }
+  return freq;
 }
 
 // Desafio 7
 
-function calculateDistance(mouse,cat){
-  let dmouseCat = Math.abs(mouse-cat);
-  return dmouseCat
+function calculateDistance(mouse,cat) {
+  let dmouseCat = Math.abs(mouse - cat);
+  return dmouseCat;
 }
 
-function catAndMouse(mouse, cat1, cat2) {
-  let dmouseCat1 = calculateDistance(mouse,cat1);
-  let dmouseCat2 = calculateDistance(mouse,cat2);
-  let statement = (dmouseCat1 == dmouseCat2)?'os gatos trombam e o rato foge': (dmouseCat1 > dmouseCat2)? 'cat2': 'cat1';
+function catAndMouse(mouse, cat1 , cat2) {
+  let dmouseCat1 = calculateDistance(mouse , cat1);
+  let dmouseCat2 = calculateDistance(mouse , cat2);
+  let statement = '';
+  if(dmouseCat1 === dmouseCat2) {
+    statement = 'os gatos trombam e o rato foge';
+  }   
+  if (dmouseCat1 > dmouseCat2) {
+    statement = 'cat2';
+  } else {
+    statement = 'cat1'
+  }
   return statement;  
 }
 
-
 // Desafio 8
-function compare(number){
+function compare(number) {
   let statement = 'bug!';
-  if (number%3 ==0){
+  if (number % 3 === 0) {
     statement = 'fizz';
-    } 
-  if (number%5 ==0) {
+  } 
+  if (number % 5 === 0) {
     statement = 'buzz';
-      }
-  if (number%3  == 0 && number%5 == 0){
-    statement = 'fizzBuzz';
   }
-   
+  if (number % 3  === 0 && number % 5 === 0) {
+    statement = 'fizzBuzz';
+  } 
   return statement;   
 }
 function fizzBuzz(array) {
-  let list = [] 
-  for (i in array){
+  let list = [];
+  for (i in array) {
     let result = compare(array[i])
     list.push(result);
   }
@@ -85,49 +93,49 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function isVogal(character){
-  let vogais= ['a','e','i','o','u'];
-  let result= vogais.indexOf(character)+1;
-  if(result > 0){
+function isVogal(character) {
+  let vogais = ['a' , 'e' , 'i' , 'o' ,' u'];
+  let result = vogais.indexOf(character) + 1;
+  if(result > 0) {
     return result;
-  } else{
+  } else {
     return character;
   }
 }
- function encode(text){
-   let newText = '';
-  for (i = 0;i < text.length; i+=1){
-   let index =isVogal(text[i]);
-   newText += index;
-  }
-  return newText;
+function encode(text) {
+  let newText = '';
+  for (i = 0;i < text.length; i += 1) {
+     let index = isVogal(text[i]);
+     newText += index;
+   }
+ return newText;
 }
-function isNumber(number){
-  let numbersList = ['1','2','3','4','5'];
+function isNumber(number) {
+  let numbersList = ['1' , '2' , '3' , '4' , '5'];
   let vogais= ['a','e','i','o','u'];
   let result= numbersList.indexOf(number);
-  if(result >= 0){
+  if(result >= 0) {
     let resultFromVogal = vogais[result]
     return resultFromVogal;
-  } else{
+  } else {
     return number;
   }
 }
 // console.log(isNumber('!'));
- function decode(text){
-   let newText = '';
-  for (i = 0;i < text.length; i+=1){
-   let index =isNumber(text[i]);
-   newText += index;
+function decode(text) {
+  let newText = '';
+  for (i = 0;i < text.length; i += 1) {
+    let index = isNumber(text[i]);
+    newText += index;
   }
   return newText;
 }
 // Desafio 10
-function techList(array,name){
+function techList(array,name) {
   array.sort();
   let listWithObjects = [];
   objectInLIst = {};
-  if (array.length == 0){
+  if (array.length == 0) {
     return 'Vazio!';
   } else {
     for(i in array) {
@@ -136,7 +144,7 @@ function techList(array,name){
           'name' : name,
       });
     }
-    return listWithObjects;  
+  return listWithObjects;  
   }
 }
 
