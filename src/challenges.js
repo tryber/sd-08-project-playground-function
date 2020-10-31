2525ba5aeb84d6dc480401d71333995ea71f778d
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-31 19:50:16 -0300

    Colocando parametro

--- src/challenges.js
+++ src/challenges.js
@@ -126,7 +126,7 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numberPhone) {
   // seu código aqui
   let = fone = '';
   if (numberPhone.length > 11) {
@@ -160,7 +160,7 @@ function generatePhoneNumber() {
       fone += numberPhone[i];
     }
   }
-  
+
   return fone;
 }
 
