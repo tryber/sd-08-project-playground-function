09ba7e6c4ee9365d53974b389741cc397d0a4312
Bruno <brunocmoraes97@gmail.com>
2020-11-01 12:33:58 -0300

    Segunda tentativa para corrigir exercício 7

--- src/challenges.js
+++ src/challenges.js
@@ -52,7 +52,8 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2){ 
+function catAndMouse(mouse, cat1, cat2){
+  Math.abs(mouse);
   if(Math.abs(cat2 - mouse) == Math.abs(cat1 - mouse)) {
   return 'Os gatos trombam e o rato foge';
   }else if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
