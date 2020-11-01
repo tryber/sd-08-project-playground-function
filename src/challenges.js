0bbc7549024aca292eaa3aaf0b14f00676ebf90c
Patrícia <silva.patricia.v@gmail.com>
2020-11-01 19:24:34 -0300

    requirement 7 updated

--- src/challenges.js
+++ src/challenges.js
@@ -46,12 +46,12 @@ function highestCount(meuArray) {
 }
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let possible1 = math.abs(cat1 - mouse);
-  let possible2 = math.abs(cat2 - mouse);
+  let dist1 = Math.abs(cat1 - mouse);
+  let dist2 = Math.abs(cat2 - mouse);
   let string = "os gatos trombam e o rato foge";
-  if (possible1 < possible2) return cat1;
-  if (possible2 < possible1) return cat2;
-  if (possible1 == possible2) return string;
+  if (dist1 < dist2) return cat1;
+  if (dist2 < dist1) return cat2;
+  if (dist1 === dist2) return string;
 }
 
 // Desafio 8
