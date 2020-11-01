4d830e2b71a97b69286eddd2a17abda25fbbdf9e
enicio <jessandro42@gmail.com>
2020-11-01 01:01:21 -0300

    Desafio 12 - com ajuste 10

--- src/challenges.js
+++ src/challenges.js
@@ -248,13 +248,13 @@ function triangleCheck(lineA, lineB, lineC) {
 
   if ( ( lineA > sideANeg  && lineB > sideBNeg && lineC > sideCNeg ) ||
        ( lineA < sideAPlus && lineB < sideBPlus && lineC < sideCPlus  ) ) {
-        return true
-      } else {
         return false
+      } else {
+        return true
       }
 
 }
-console.log(triangleCheck(10, 14, 8))
+console.log(triangleCheck(16, 9, 2))
 
 
 
