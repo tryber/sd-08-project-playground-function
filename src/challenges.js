17f99417f8c622ab8b9cd30a9d2a86b0645a5076
Patrícia <silva.patricia.v@gmail.com>
2020-11-01 16:37:35 -0300

    requirement 8 updated

--- src/challenges.js
+++ src/challenges.js
@@ -53,19 +53,19 @@ function catAndMouse() {
 function fizzBuzz(array) {
   let arrayFizzBuzz = [];
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] % 3 === 0) {
-      arrayFizzBuzz.push("fizz");
-    } else if (array[index] % 5 === 0) {
-      arrayFizzBuzz.push("buzz");
-    } else if (array[index] % 3 === 0 && [index] % 5 === 0) {
+    if (array[index] % 3 == 0 && array[index] % 5 == 0) {
       arrayFizzBuzz.push("fizbuzz");
+    } else if (array[index] % 3 == 0) {
+      arrayFizzBuzz.push("fizz");
+    } else if (array[index] % 5 == 0) {
+      arrayFizzBuzz.push("buzz");
     } else {
       arrayFizzBuzz.push("bug!");
     }
   }
   return arrayFizzBuzz;
 }
-
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
