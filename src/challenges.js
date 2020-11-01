d7772aa98b48ed18c8918b65d2a4cf88a26db438
Bruno <brunocmoraes97@gmail.com>
2020-11-01 12:11:24 -0300

    Adiciona correção para o exercício 7

--- src/challenges.js
+++ src/challenges.js
@@ -53,17 +53,15 @@ function highestCount() {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2){ 
-  let winner;
   if((cat2 - mouse) == (cat1 - mouse)) {
-  winner = 'Os gatos trombam e o rato foge'
+  return 'Os gatos trombam e o rato foge';
   }else if((cat1 - mouse) < (cat2 - mouse)){
-    winner = 'cat1';
+    return 'cat1';
   }else{
-    winner = 'cat2';
+    return 'cat2';
   }
-  return winner;
 }  
-console.log(catAndMouse(1, 4, 4));
+console.log(catAndMouse(1, 7, 4));
 
 // Desafio 8
 function fizzBuzz() {
