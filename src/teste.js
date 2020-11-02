8571d168bb1bc2fa5639ef2760370db56d4d9761
Bruno <brunocmoraes97@gmail.com>
2020-11-02 13:49:45 -0300

    Correção para o exercício 7

--- src/teste.js
+++ src/teste.js
@@ -1,42 +1,12 @@
-// Desafio 9
-function encode(forward) {
-  let word = "";
-  for(let index = 0; index < forward.length; index++){
-    if(forward[index] == 'a'){
-      word += 1;
-    }else if(forward[index] == 'e'){
-      word += 2;
-    }else if(forward[index] == 'i'){
-      word += 3;
-    }else if(forward[index] == 'o'){
-      word += 4;
-    }else if(forward[index] == 'u'){
-      word += 5;
-    }else{
-     word += forward[index];
-    }
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2){
+  if(Math.abs(cat2 - mouse) == Math.abs(cat1 - mouse)) {
+  return 'Os gatos trombam e o rato foge';
+  }else if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return 'cat1';
+  }else{
+    return 'cat2';
   }
-  return word;
-}
-console.log(encode('hi there!'));
+}  
 
-function decode(behind) {
-  let reverse = ""
-  for(let index = 0; index < behind.length; index++){
-    if(behind[index] == '1'){
-      reverse += 'a';
-    }else if(behind[index] == '2'){
-      reverse += 'e';
-    }else if(behind[index] == '3'){
-      reverse += 'i';
-    }else if(behind[index] == '4'){
-      reverse += 'o';
-    }else if(behind[index] == '5'){
-      reverse += 'u';
-    }else{
-     reverse += behind[index];
-    }
-  }
-  return reverse;
-}
-console.log(decode('h3 th2r2!'));
+console.log(catAndMouse(1, 2, 2));
