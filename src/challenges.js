53af901468bdf39a3e07c8196c687141a57fb431
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-02 18:50:14 -0300

    Voltando a versão

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,7 @@ function calcArea(base, height) {
 function splitSentence(string) {
   return string.split(' ', string.length);
 }
-
+console.log(splitSentence('Alo alo alo alo alo alo'));
   // seu código aqui
 
 
@@ -33,7 +33,7 @@ function concatName(array) {
   return ultimoArray+', '+primeiroArray;
   // seu código aqui
 }
-
+console.log(concatName(['douglas', 'Ferreira', 'Pinto']))
 
 
 
@@ -43,7 +43,7 @@ function footballPoints(wins, ties) {
   // seu código aqui
 
 }
-
+console.log(footballPoints(5,2))
 
 // Desafio 6
 function highestCount(array) {
@@ -62,7 +62,7 @@ function highestCount(array) {
   return contador;
   // seu código aqui
 }
-
+console.log(highestCount([1,2,5,6,9,9, 11, 11]))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -79,7 +79,7 @@ function catAndMouse(mouse, cat1, cat2) {
   return catAndMouse
   // seu código aqui
 }
-
+console.log(catAndMouse(6,2,10))
 
 // Desafio 8
 function fizzBuzz(array) {
@@ -99,62 +99,16 @@ function fizzBuzz(array) {
   return frase;
   // seu código aqui
 }
-
+console.log(fizzBuzz([3,5,15,2,30]));
 
 // Desafio 9
-function encode(string) {
-  let frase = [];
-  let fraseCod = '';
-  
-  for(let key in string){
-    if(string[key] == 'a'){
-      frase.push('1')
-    }else if(string[key] == 'e'){
-      frase.push('2')
-    }else if(string[key] == 'i'){
-      frase.push('3')
-    }else if(string[key] == 'o'){
-      frase.push('4')
-    }else if(string[key] == 'u'){
-      frase.push('5')
-    }else{
-      fraseCod.push(string[key]);
-    }
- }
- for(let key in frase){
-   fraseCod += frase[key];
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
- }
- return frase;
-} 
-console.log(encode('oi'))
-
-function decode(string) {
-  let frase = [];
-  let fraseCod = '';
-  
-  for(let key in string){
-    if(string[key] == '1'){
-      frase.push('a')
-    }else if(string[key] == '2'){
-      frase.push('e')
-    }else if(string[key] == '3'){
-      frase.push('i')
-    }else if(string[key] == '4'){
-      frase.push('o')
-    }else if(string[key] == '5'){
-      frase.push('u')
-    }else{
-      fraseCod.push(string[key]);
-    }
- }
- for(let key in frase){
-   fraseCod += frase[key];
-
- }
- return frase;
-} 
-console.log(decode('oi'))
 // Desafio 10
 function techList() {
   // seu código aqui
