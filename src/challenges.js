87238bab13b8c1c447437374602b590a1effbadf
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-02 18:24:52 -0300

    :up:exercicio 9

--- src/challenges.js
+++ src/challenges.js
@@ -134,9 +134,17 @@ function fizzBuzz(param) {
 function encode(string) {
   // seu código aqui
   let codeString ='';
-  for(i = 0; i <string.length; i++){
+  for(i = 0; i < string.length; i++){
     if(string[i] === 'a'){
         codeString += 1;
+    }else if(string[i] === 'e'){
+        codeString += 2;
+    }else if(string[i] === 'i'){
+        codeString += 3;
+    }else if(string[i] === 'o'){
+        codeString += 4;
+    }else if(string[i] === 'u'){
+        codeString += 5;
     }else{
       codeString += string[i];
     }
@@ -144,16 +152,38 @@ function encode(string) {
   return codeString;
 };
 
-console.log(encode('parametro'));
 
 
 
 
 
 
-function decode() {
+
+function decode(string) {
   // seu código aqui
+  let codeString ='';
+  for(i = 0; i < string.length; i++){
+    if(string[i] === '1'){
+        codeString += 'a';
+    }else if(string[i] === '2'){
+        codeString += 'e';
+    }else if(string[i] === '3'){
+        codeString += 'i';
+    }else if(string[i] === '4'){
+        codeString += 'o';
+    }else if(string[i] === '5'){
+        codeString += 'u';
+    }else{
+      codeString += string[i];
+    }
+  };
+  return codeString;
 }
+
+
+
+
+
 
 // Desafio 10
 function techList() {
