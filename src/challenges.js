be199765cb60a4376fa74119f4e32868f0603dbd
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-02 18:42:42 -0300

    Teste ex 9

--- src/challenges.js
+++ src/challenges.js
@@ -102,13 +102,59 @@ function fizzBuzz(array) {
 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+function encode(string) {
+  let frase = [];
+  let fraseCod = '';
+  
+  for(let key in string){
+    if(string[key] == 'a'){
+      frase.push('1')
+    }else if(string[key] == 'e'){
+      frase.push('2')
+    }else if(string[key] == 'i'){
+      frase.push('3')
+    }else if(string[key] == 'o'){
+      frase.push('4')
+    }else if(string[key] == 'u'){
+      frase.push('5')
+    }else{
+      fraseCod.push(string[key]);
+    }
+ }
+ for(let key in frase){
+   fraseCod += frase[key];
 
+ }
+ return frase;
+} 
+console.log(encode('oi'))
+
+function decode(string) {
+  let frase = [];
+  let fraseCod = '';
+  
+  for(let key in string){
+    if(string[key] == '1'){
+      frase.push('a')
+    }else if(string[key] == '2'){
+      frase.push('e')
+    }else if(string[key] == '3'){
+      frase.push('i')
+    }else if(string[key] == '4'){
+      frase.push('o')
+    }else if(string[key] == '5'){
+      frase.push('u')
+    }else{
+      fraseCod.push(string[key]);
+    }
+ }
+ for(let key in frase){
+   fraseCod += frase[key];
+
+ }
+ return frase;
+} 
+console.log(decode('oi'))
 // Desafio 10
 function techList() {
   // seu código aqui
