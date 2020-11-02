23f4db5e8c1471a10b8d0e37b14ccd332dcc32d0
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-02 13:49:01 -0300

    Adding exercises 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,103 +1,129 @@
 // Desafio 1
-function compareTrue(value1,value2) {
-
-    if(value1 && value2 === true) {
-            return true;
-        } else {
-            return false;
-        }
+function compareTrue(value1, value2) {
+  if (value1 && value2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea(base,height) {
-    return(base*height)/2;
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(word) {
-  return word.split (" ");
+  return word.split(" ");
 }
 
 // Desafio 4
 function concatName(arrNames) {
-  let lastPosition = arrNames.length -1;
+  let lastPosition = arrNames.length - 1;
   let firstName = arrNames[0];
-  let lastName = arrNames [lastPosition];
+  let lastName = arrNames[lastPosition];
   return lastName + ", " + firstName;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-    let totalpoints = (wins*3) + ties;
-    return totalpoints;
-
+function footballPoints(wins, ties) {
+  let totalpoints = wins * 3 + ties;
+  return totalpoints;
 }
 
 // Desafio 6
 function highestCount(arrNumbers) {
-    let highestNumber = 0;
-    let highestRepeat = 0;
+  let highestNumber = 0;
+  let highestRepeat = 0;
 
-    for(let i = 0;i < arrNumbers.length;i+=1){
-        if(arrNumbers[i] > highestNumber){
-            highestNumber = arrNumbers[i];
-            highestRepeat = 0;
-        }
-        if(arrNumbers[i] == highestNumber){
-            highestRepeat += 1;
-        }         
+  for (let i = 0; i < arrNumbers.length; i += 1) {
+    if (arrNumbers[i] > highestNumber) {
+      highestNumber = arrNumbers[i];
+      highestRepeat = 0;
     }
-       
-  return highestRepeat;
+    if (arrNumbers[i] == highestNumber) {
+      highestRepeat += 1;
+    }
+  }
 
+  return highestRepeat;
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
-     
-     let distanceCat1 = Math.abs(cat1 - mouse);
-     let distanceCat2 = Math.abs(cat2 - mouse);
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
 
-    if(distanceCat1 < distanceCat2){
-      return "cat1";
-    }
-    if(distanceCat2 < distanceCat1){
-      return "cat2";
-    }
-    if(distanceCat1 == distanceCat2){
-      return "os gatos trombam e o rato foge";
-    }
+  if (distanceCat1 < distanceCat2) {
+    return "cat1";
+  }
+  if (distanceCat2 < distanceCat1) {
+    return "cat2";
+  }
+  if (distanceCat1 == distanceCat2) {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
 function fizzBuzz(arrNumbers) {
-  
   let array = [];
-  
-  for(let i in arrNumbers){
-    if(arrNumbers[i] % 3 == 0 && arrNumbers[i] % 5 == 0){
+
+  for (let i in arrNumbers) {
+    if (arrNumbers[i] % 3 == 0 && arrNumbers[i] % 5 == 0) {
       array.push("fizzBuzz");
-    }
-    else if(arrNumbers[i] % 3 == 0){
+    } else if (arrNumbers[i] % 3 == 0) {
       array.push("fizz");
-    }
-    else if(arrNumbers[i] % 5 == 0){
+    } else if (arrNumbers[i] % 5 == 0) {
       array.push("buzz");
-    }
-    else{
-      array.push("bug!"); 
+    } else {
+      array.push("bug!");
     }
   }
   return array;
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
-  
+function encode(strNumbers) {
+  let encodeString = "";
+
+  for (let i = 0; i < strNumbers.length; i += 1) {
+    if (strNumbers[i] === "a") {
+      encodeString += "1";
+    } else if (strNumbers[i] === "e") {
+      encodeString += "2";
+    } else if (strNumbers[i] === "i") {
+      encodeString += "3";
+    } else if (strNumbers[i] === "o") {
+      encodeString += "4";
+    } else if (strNumbers[i] === "u") {
+      encodeString += "5";
+    } else {
+      encodeString += strNumbers[i];
+    }
+  }
+  return encodeString;
 }
-function decode() {
-  // seu código aqui
+
+function decode(strLetters) {
+  let decodeString = "";
+
+  for (let i = 0; i < strLetters.length; i += 1) {
+    if (strLetters[i] === "1") {
+      decodeString += "a";
+    } else if (strLetters[i] === "2") {
+      decodeString += "e";
+    } else if (strLetters[i] === "3") {
+      decodeString += "i";
+    } else if (strLetters[i] === "4") {
+      decodeString += "o";
+    } else if (strLetters[i] === "5") {
+      decodeString += "u";
+    } else {
+      decodeString += strLetters[i];
+    }
+  }
+  return decodeString;
 }
 
 // Desafio 10
@@ -120,7 +146,6 @@ function hydrate() {
   // seu código aqui
 }
 
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -136,4 +161,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
