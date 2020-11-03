428fd97a5edcba44913cc22d45ee9597a8b2d8eb
Márcio Daniel <50931024+marciodanielll@users.noreply.github.com>
2020-11-03 01:32:50 -0300

    Update challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,26 @@
 // Desafio 1
 function compareTrue(a, b) {
-  // seu código aqui
   if (a && b) return true
   return false
 }
 // Desafio 2
 function calcArea(base, height) {
-  // seu código aqui
   return (base * height) / 2
 }
 // Desafio 3
 function splitSentence(string) {
-  // seu código aqui
   return string.split(" ")
 }
 // Desafio 4
 function concatName(arrayNames) {
-  // seu código aqui
   return `${arrayNames.pop()}, ${arrayNames[0]}`
 }
 // Desafio 5
 function footballPoints(wins, ties) {
-  // seu código aqui
   return (wins * 3) + (ties * 1)
 }
 // Desafio 6
 function highestCount(arrayHighestCount) {
-  // seu código aqui
   let topNumber = arrayHighestCount[0]
   let cont = 0
   for (i in arrayHighestCount) {
@@ -50,7 +44,6 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 // Desafio 8
 function fizzBuzz(arrayNumbers) {
-  // seu código aqui
   let newArray = []  
   for (i in arrayNumbers) {
     if (arrayNumbers[i] % 3 === 0 && arrayNumbers[i] % 5 === 0) {
@@ -74,10 +67,8 @@ function encode(string) {
 function decode(string) {
  return string.split('1').join('a').split('2').join('e').split('3').join('i').split('4').join('o').split('5').join('u')
 }
-
 // Desafio 10
 function techList(arrTacName, name) {
-  // seu código aqui
   if(arrTacName.length === 0) return "Vazio!";
   let newArr = [];
   for (i in arrTacName) {
@@ -133,7 +124,6 @@ function hydrate(srt) {
   if(exit == 1) return `${exit} copo de água`;
   return `${exit} copos de água`;
 }
-
 module.exports = {
   calcArea,
   catAndMouse,
