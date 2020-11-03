039a07fb27bbbf753c2170dea11d7e8dca30d2e2
maym4y <jennim.almeida@gmail.com>
2020-11-03 14:38:32 -0300

    Primeiro ajuste para codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -17,16 +17,16 @@ function calcArea(base, height) {
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
-  let words = string.split(" ");
+  let words = string.split(' ');
   return words;
 }
 
 // Desafio 4
 function concatName(arrayStrings) {
   // seu código aqui
-  let retorno = "";
+  let retorno = '';
   retorno += arrayStrings[arrayStrings.length - 1];
-  retorno += ", ";
+  retorno += ', ';
   retorno += arrayStrings[0];
   return retorno;
 }
@@ -65,13 +65,13 @@ function catAndMouse(mouse, cat1, cat2) {
   let cat2ToMouse = Math.abs(cat2 - mouse);
   let winner;
   if (cat1ToMouse === cat2ToMouse) {
-    let empate = "os gatos trombam e o rato foge";
+    let empate = 'os gatos trombam e o rato foge';
     return empate;
   } else if (cat1ToMouse > cat2ToMouse) {
-    winner = "cat2";
+    winner = 'cat2';
     return winner;
-  } else if (cat2ToMouse > cat1ToMouse) {
-    winner = "cat1";
+  } else {
+    winner = 'cat1';
     return winner;
   }
 }
@@ -81,14 +81,14 @@ function fizzBuzz(array) {
   // seu código aqui
   let resultado = [];
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] % 3 == 0 && array[i] % 5 == 0) {
-      resultado.push("fizzBuzz");
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      resultado.push('fizzBuzz');
     } else if (array[i] % 3 == 0) {
-      resultado.push("fizz");
+      resultado.push('fizz');
     } else if (array[i] % 5 == 0) {
-      resultado.push("buzz");
+      resultado.push('buzz');
     } else {
-      resultado.push("bug!");
+      resultado.push('bug!');
     }
   }
   return resultado;
@@ -97,18 +97,18 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(string) {
   // seu código aqui
-  let codigo = "";
+  let codigo = '';
   for (let index = 0; index < string.length; index += 1) {
-    if (string[index] == "a") {
-      codigo += "1";
-    } else if (string[index] == "e") {
-      codigo += "2";
-    } else if (string[index] == "i") {
-      codigo += "3";
-    } else if (string[index] == "o") {
-      codigo += "4";
-    } else if (string[index] == "u") {
-      codigo += "5";
+    if (string[index] === 'a') {
+      codigo += '1';
+    } else if (string[index] === 'e') {
+      codigo += '2';
+    } else if (string[index] === 'i') {
+      codigo += '3';
+    } else if (string[index] === 'o') {
+      codigo += '4';
+    } else if (string[index] === 'u') {
+      codigo += '5';
     } else {
       codigo += string[index];
     }
@@ -118,20 +118,15 @@ function encode(string) {
 
 function decode(codigo) {
   // seu código aqui
-  let frase = "";
+  let frase = '';
   for (let index = 0; index < codigo.length; index += 1) {
-    if (codigo[index] == "1") {
-      frase += "a";
-    } else if (codigo[index] == "2") {
-      frase += "e";
-    } else if (codigo[index] == "3") {
-      frase += "i";
-    } else if (codigo[index] == "4") {
-      frase += "o";
-    } else if (codigo[index] == "5") {
-      frase += "u";
-    } else {
-      frase += codigo[index];
+    if (codigo[index] == '1') {
+      frase += 'a';
+    } else if (codigo[index] == '2') {
+      frase += 'e';
+    } else if (codigo[index] == '3') {
+      frase += 'i';
+    } else if (codigo[index] == '4') {
     }
   }
   return frase;
@@ -141,7 +136,7 @@ function decode(codigo) {
 function techList(techArray, seuNome) {
   // seu código aqui
   let array = techArray.sort();
-  let vazio = "Vazio!";
+  let vazio = 'Vazio!';
   let saida = [];
 
   if (techArray.length > 0) {
@@ -159,35 +154,34 @@ function techList(techArray, seuNome) {
 
 // Desafio 11
 function generatePhoneNumber(numerico) {
-  // seu código 
-  let telefone = "";
-  let erro = "Array com tamanho incorreto."
-  let impossivel = "não é possível gerar um número de telefone com esses valores"
+  let telefone = '';
+  let erro = 'Array com tamanho incorreto.';
+  let impossivel = 'não é possível gerar um número de telefone com esses valores';
   let anterior = 0;
   let proximo = 0;
-  if (numerico.length == 11){
+  if (numerico.length == 11) {
       let sorted = numerico.slice().sort();
-      for (let key = 0; key < numerico.length; key += 1){
+      for (let key = 0; key < numerico.length; key += 1) {
         letra = key;
         petra = key;
         anterior = letra -= 1;
         proximo = petra += 1;
-          if (sorted[key] < 0 || sorted[key] > 9){
+          if (sorted[key] < 0 || sorted[key] > 9) {
               return impossivel;
-          } else if (sorted[key] == sorted[anterior] && sorted[key] == sorted[proximo]){
+          } else if (sorted[key] == sorted[anterior] && sorted[key] == sorted[proximo]) {
               return impossivel;
-          } else if (key == 0){
-              telefone += "(";
+          } else if (key == 0) {
+              telefone += '(';
               telefone += numerico[key];
-          } else if (key == 1){
+          } else if (key == 1) {
               telefone += numerico[key];
-              telefone += ")";
-              telefone += " ";
-          } else if (key > 1 && key < 6){
+              telefone += ')';
+              telefone += ' ';
+          } else if (key > 1 && key < 6) {
               telefone += numerico[key];   
           } else if (key == 6) { 
               telefone += numerico[key]
-              telefone += "-"
+              telefone += '-'
           } else if (key >= 7) {
               telefone += numerico[key];
           }
