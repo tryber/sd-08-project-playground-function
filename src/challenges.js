c18dbccfabaf45b4925349708b398b4610f75b7b
Bruna <92brunacampos@gmail.com>
2020-11-03 18:09:51 -0300

    exercicio 8.7

--- src/challenges.js
+++ src/challenges.js
@@ -51,11 +51,11 @@ function catAndMouse() {
 function fizzBuzz(array) {
   let result = [];
     for (i = 0; i < array.length - 1; i += 1) {
-      if (i % 3 == 0 && i % 5 == 0) {
+      if (i % 3 = 0 && i % 5 = 0) {
         result.push ("fizzBuzz");
-      } else if (i % 3 == 0) {
+      } else if (i % 3 = 0) {
         result.push ("fizz");
-      } else if (i % 5 == 0) {
+      } else if (i % 5 = 0) {
         result.push ("buzz");
       } else {
         result.push ("bug!");
