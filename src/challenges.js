c54ad36e75fca9917547dd57a774d18a2ab94687
Uibirá <usbarreto@gmail.com>
2020-11-03 02:53:37 -0300

    Desafio 07: correção no cálculo de distâncias

--- src/challenges.js
+++ src/challenges.js
@@ -58,10 +58,10 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (mouse - cat1 < mouse - cat2){
+  if (Math.abs (mouse - cat1) < Math.abs (mouse - cat2)){
     return "cat1"
   }
-  else if (mouse - cat1 == mouse - cat2){
+  else if (Math.abs (mouse - cat1) == Math.abs (mouse - cat2)){
     return "os gatos trombam e o rato foge"
   }
   else {
