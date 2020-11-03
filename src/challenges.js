c5b118e028b8b7eb3b7bfcfb1604713a9a8434b2
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-03 20:27:47 -0300

    Alterando texto no return desafio 11

--- src/challenges.js
+++ src/challenges.js
@@ -167,7 +167,7 @@ function generatePhoneNumber(arrayNum) {
     let numtel = '';
 
     if (arrayNum.length !== 11) {
-        return 'array com tamanho incorreto.';
+        return 'Array com tamanho incorreto.';
     }
     for (let j = 0; j < arrayNum.length; j += 1) {
         let contador = 0;
