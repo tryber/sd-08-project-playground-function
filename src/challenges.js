a663ac881645f33ae65991f023fd8546197155c8
Victor Costa <victordiricoo@gmail.com>
2020-11-03 20:48:27 -0300

    voltei no desafio4

--- src/challenges.js
+++ src/challenges.js
@@ -18,8 +18,8 @@ function splitSentence(a = "go Trybe") {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(param = [", tudo bem?", "sei la", "como vai?", "oi"]) {
+ return param[3].concat(param[0]);
 }
 
 // Desafio 5
