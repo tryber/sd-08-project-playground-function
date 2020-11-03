348dea86ea14ec2a2112542266053c1393aa4ae1
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-11-03 01:37:24 -0300

    fim

--- src/challenges.js
+++ src/challenges.js
@@ -132,7 +132,7 @@ function buildPhoneNumber(array) {
     if (i === 6) {
       phoneNumber += `${array[i]}-`;
     } else {
-    phoneNumber += `${array[i]}`;
+      phoneNumber += `${array[i]}`;
     }
   }
   return phoneNumber;
@@ -149,7 +149,6 @@ function generatePhoneNumber(arrayPhone) {
   }
   return buildPhoneNumber(arrayPhone);
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
