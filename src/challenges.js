91233bb9825e2dd8a5821525f1fde0effdf83364
miguelbrn <miiguelalb@gmail.com>
2020-11-03 20:51:40 -0300

    90% complete

--- src/challenges.js
+++ src/challenges.js
@@ -101,10 +101,33 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]))
 // Desafio 9
 function encode(str) {
   // seu código aqui
-  return str.replaceall('e', '3')
+  let codificar = str
+    .split('a')
+    .join('1')
+    .split('e')
+    .join('2')
+    .split('i')
+    .join('3')
+    .split('o')
+    .join('4')
+    .split('u')
+    .join('5');
+  return codificar;
 }
-function decode() {
+function decode(str) {
   // seu código aqui
+  let decodifcar = str
+    .split('1')
+    .join('a')
+    .split('2')
+    .join('e')
+    .split('3')
+    .join('i')
+    .split('4')
+    .join('o')
+    .split('5')
+    .join('u');
+  return decodifcar
 }
 
 console.log(encode('hi there!'))
