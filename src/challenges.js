79be65099c7e705534d6c5c23bd5e8e70c48f5af
Bruno Wedley <brunowedley27@gmail.com>
2020-11-03 13:20:32 -0300

    adequação de condição no desafio 12

--- src/challenges.js
+++ src/challenges.js
@@ -183,14 +183,14 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (lineA < lineB + lineC || lineB < lineA + lineC || lineC < lineA + lineB){
-    if(lineA > Math.abs(lineB - lineC) || lineB > Math.abs(lineA - lineC) || lineC > Math.abs(lineA - lineB)){
+  if ((lineA < lineB + lineC) && (lineA > Math.abs(lineB - lineC))){
     return true;
-    }
+  } else {
+    return false;
   }
   // seu código aqui
 }
-// console.log(triangleCheck(10, 14, 8))
+ //console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
 function hydrate(string) {
