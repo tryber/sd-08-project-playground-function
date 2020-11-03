5aff9d282f72955768a90253e62ab3ba19c52817
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-03 12:12:15 -0300

    highest count

--- src/exercicios.js
+++ src/exercicios.js
@@ -1,22 +1,12 @@
-/*7 - Caça ao rato
-Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2, e que ambos estão atrás de um rato chamado mouse. 
-Imagine que cada um dos três animais está em uma posição representada por um número.
-
-Sabendo disso, crie uma função chamada catAndMouse que, ao receber a posição de mouse, cat1 e cat2, nessa ordem, 
-calcule as distâncias entre o rato e os gatos e retorne qual dos felinos irá alcançar o rato primeiro 
-(sendo aquele que estará mais perto).
-
-Exemplo: caso o gato cat2 esteja a 2 unidades de distância do rato, e cat1 esteja a 3 unidades, sua função
- deverá retornar "cat2".
-
-Caso os gatos estejam na mesma distância do rato, a função deverá retornar a string "os gatos trombam e o rato foge".*/
-
 
 function catAndMouse(mouse, cat1, cat2) {
+    let distanciaGato1 = Math.abs(cat1 - mouse);
+    let distanciaGato2 = Math.abs(cat2 - mouse);
     let catCloser = '';
-    if ((cat1 - mouse) < (cat2 - mouse)) {
+
+    if (distanciaGato1 < distanciaGato2) {
         catCloser = 'cat1';
-    } else if ((cat1 - mouse) > (cat2 - mouse)) {
+    } else if (distanciaGato1 > distanciaGato2) {
         catCloser = 'cat2';
     } else {
         catCloser = 'Os gatos trombam e o rato foge';
@@ -24,4 +14,29 @@ function catAndMouse(mouse, cat1, cat2) {
     return catCloser
 }
 
-console.log(catAndMouse(2, 10, 12))
+
+
+
+function catAndMouse(mouse, cat1, cat2) {
+    let catCloser = '';
+    let distanciaGato1 = cat1 - mouse;
+    let distanciaGato2 = cat2 - mouse;
+
+    if (distanciaGato1 < 0) {
+        distanciaGato1 = distanciaGato1 * (-1)
+    }
+    if (distanciaGato2 < 0) {
+        distanciaGato2 = distanciaGato2 * (-1)
+    }
+
+    if (distanciaGato1 < distanciaGato2) {
+        catCloser = 'cat1';
+    } else if (distanciaGato1 > distanciaGato2) {
+        catCloser = 'cat2';
+    } else {
+        catCloser = 'Os gatos trombam e o rato foge';
+    }
+    return catCloser
+}
+
+
