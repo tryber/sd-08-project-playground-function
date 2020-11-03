7880ae06eee8ec0b6ce9edcb8ca8ef0df1748dce
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-02 23:59:12 -0300

    Fixing 07

--- src/challenges.js
+++ src/challenges.js
@@ -33,12 +33,12 @@ function highestCount() {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (mouse - cat1 < mouse - cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2) {
     return("cat1");
-  } else if (mouse - cat1 > mouse - cat2) {
+  } else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2) {
     return("cat2");
-  } else if (mouse - cat1 == mouse - cat2) {
-    return("os gatos trombam e o rato foge")
+  } else if (Math.abs(mouse - cat1) == Math.abs(mouse - cat2) {
+    return("os gatos trombam e o rato foge");
   }
 }
 
