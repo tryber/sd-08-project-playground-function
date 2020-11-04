d26ed03e68099d325ecbd8500f803402bf0e3148
Antônio Schappo <antonioschappo@gmail.com>
2020-11-04 19:39:30 -0300

    Try ex 10

--- src/challenges.js
+++ src/challenges.js
@@ -170,24 +170,21 @@ function decode(string) {
 }
 
 
-/***9 - Codifique e Decodifique**
--
-Crie duas funções: a primeira deverá se chamar `encode` e, ao receber uma string como parâmetro, deverá trocar todas as vogais minúsculas por números, de acordo com o formato a seguir:
-
-a -> 1 \
-e -> 2 \
-i -> 3 \
-o -> 4 \
-u -> 5
-
-Ou seja, caso o parâmetro de `encode` seja `"hi there!"`, o retorno deverá ser `"h3 th2r2!"`.
-
-A segunda função deverá se chamar `decode` e faz o contrário de `encode` - ou seja, recebe uma string contendo números no lugar de letras minúsculas e retornará uma string com vogais minúsculas no lugar dos números (então, caso o parâmetro de `decode` seja `"h3 th2r2!"`, o retorno deverá ser `"hi there!"`). */
 
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let outcome = [];
+  array.sort();
+  for(let i = 0; i< array.length; i+=1){
+    if(array === []){
+      outcome = 'Vazio!'
+    }else if(array[i]){
+      let tec = {tech: array[i], 'name': name};
+      outcome.push(tec)
+    }
+  }return outcome;
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
