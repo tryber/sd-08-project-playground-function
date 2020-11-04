a22a27e2a02599bd086f5bf60299a671f251b7ac
Thais Quintela <thais.s.quintela@gmail.com>
2020-11-04 18:40:42 -0300

    Nova correcao requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -132,9 +132,9 @@ function techList(techArray, name) {
 // Desafio 11
 function generatePhoneNumber(numbers) {
   let index;
-  for (index in numbers) {
-    function repetition(number) {
-    return number == numbers[index];
+  function repetition(number) {
+    for (index in numbers) {
+      return number == numbers[index];
     }
   }
   let num = numbers.filter(repetition)
@@ -147,7 +147,7 @@ function generatePhoneNumber(numbers) {
   else {
     return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
   }
-}
+} console.log(generatePhoneNumber([]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
