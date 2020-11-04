ba4bf237335f117a0e687b1151ca1401e3eddf8f
Samuel <samuelvanguarda@gmail.com>
2020-11-04 14:57:22 -0300

    Requisito 6 - Atualizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,3 @@
-//Tive ajuda do Bruno Cândido para rosolver algumas partes do código.
-
-// Desafio 1
-function compareTrue(bool1, bool2) {
-  if(bool1 === true && bool2 === true){
-    return true; 
-  }else{
-    return false;
-  }
-}
-console.log(compareTrue(true, true));
-
-
-// Desafio 2
-function calcArea(base, height) {
-  let areaTriangulo;
-  areaTriangulo = (base * height) / 2;
-  return areaTriangulo;
-}
-console.log(calcArea(4,6));
-
-
-// Desafio 3
-//Bibliografia: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254#:~:text=O%20m%C3%A9todo%20split()%20divide,das%20substrings%20resultantes%20no%20array.
-function splitSentence(string) {
-  let dividido = [];
-  dividido = string.split(" ");
-  return dividido;
-}
-console.log(splitSentence("ola povo"));
-
-
-// Desafio 4
 function concatName(array) {
   let invertido = [];
   for(let i = 0; i < array.length; i++){
@@ -39,16 +6,12 @@ function concatName(array) {
   return(invertido[invertido.length-1] + ", " + invertido[0])
 } 
 console.log(concatName(['pedro', 'joão' , 'rosa', 'maria']));
-
-
 // Desafio 5
 function footballPoints(wins, ties) {
   let points = (wins *3) + ties;
   return points;
 } 
 console.log(footballPoints(3, 2));
-
-
 // Desafio 6
 function highestCount(arrayNumeros) {
   let maior = 0;
@@ -56,7 +19,7 @@ function highestCount(arrayNumeros) {
   for (let i = 0; i < arrayNumeros.length; i ++) {
     if (arrayNumeros[i] > maior){
       maior = arrayNumeros[i];
-    
+
   for (let i = 0; i < arrayNumeros.length; i ++) {
     if (arrayNumeros[i] == maior){
       vezes += 1;
@@ -65,7 +28,6 @@ function highestCount(arrayNumeros) {
   return vezes;
 }
   console.log(highestCount([3, 2, 3, 1, 2]));
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distancia1 = Math.abs(cat1 - mouse);
@@ -81,7 +43,6 @@ function catAndMouse(mouse, cat1, cat2) {
   return gatoVencedor;
 }
 console.log(catAndMouse(2, 3, 3))
-
 // Desafio 8
 function fizzBuzz(array) {
   let divisiveis = [];
@@ -99,7 +60,6 @@ function fizzBuzz(array) {
   return divisiveis;
 }
 console.log(fizzBuzz([2, 3, 15, 30]));
-
 // Desafio 9
 function encode(stringQualquer) {
   let concatenado = '';
@@ -121,7 +81,6 @@ function encode(stringQualquer) {
   return concatenado;
 }
 console.log(encode('samuel ramos campos'))
-
 function decode(codigo) {
   let transcrito = '';
   for(let i = 0; i < codigo.length; i++){
@@ -142,7 +101,6 @@ function decode(codigo) {
   return transcrito;
 }
 console.log((decode('s1m52l r1m4s c1mp4s')))
-
 // Desafio 10
 function techList(tecnologias, name) {
   let objetos = [];
@@ -164,18 +122,14 @@ console.log(techList(['css', 'java', 'html', 'python'], "samuel"));
 function generatePhoneNumber() {
   // seu código aqui
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
 }
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
-
-
 module.exports = {
   calcArea,
   catAndMouse,
