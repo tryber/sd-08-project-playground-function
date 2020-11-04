1a1a6f64cd1951c83e25b3b1d2368760c16efab0
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-04 01:09:20 -0300

    C11 tentando resolver problemas do desafio 1 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,9 @@
 // Desafio 1
-function compareTrue(num1, num2) {
-  if (num1 && num2 !== 0) {
-    return false;
-  } else {
+function compareTrue(bool1, bool2) {
+  if (bool1 && bool2 === true) {
     return true;
+  } else {
+    return false;
   }
 }
 
