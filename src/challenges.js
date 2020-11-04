7e71215101d156fb6140e0d5eef00b3e3c91ca26
ok <fabnhm@hotmail.com>
2020-11-03 23:45:18 -0300

    Feito requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let arrayOfStrings = string.split(' ');
+  return arrayOfStrings;
 }
 
 // Desafio 4
