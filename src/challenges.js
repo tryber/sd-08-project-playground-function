31e4d5dfd2e1bbf3e138608454d20df919880b14
Cristiano <cristianoroberto@outlook.com>
2020-11-04 09:06:42 -0300

    Tentativas no Des. 4

--- src/challenges.js
+++ src/challenges.js
@@ -21,8 +21,10 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(arrayOfStrings) {
+    let arrayOfStrings = [];
     return [arrayOfStrings.lenght - 1] + "," + arrayOfStrings[0];
 }
+//console.log(concatName("Lucas", "Cassiano", "Ferraz", "Paollillo"))
 
 // Desafio 5
 function footballPoints(wins, ties) {
