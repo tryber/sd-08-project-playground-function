45712533412e052f0cebde29f008246f4a2c06ea
jnpefo <jnpefo@gmail.com>
2020-11-05 14:00:53 -0300

    Entregando projeto

--- src/challenges.js
+++ src/challenges.js
@@ -167,45 +167,49 @@ function generatePhoneNumber(number) {
   let cMaior = 0;
 
   if (number.length != 11) {
-      return 'Array com tamanho incorreto.';
+    return 'Array com tamanho incorreto.';
   }
   for (let i = 0; i < number.length; i += 1) {
-      cMaior = 0;
-      if (number[i] < 0 || number[i] > 9) {
-          return 'não é possível gerar um número de telefone com esses valores';
+    cMaior = 0;
+    if (number[i] < 0 || number[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let c = 0; c < number.length; c += 1) {
+      if (number[i] === number[c]) {
+        cMaior += 1;
       }
-      for (let c = 0; c < number.length; c += 1) {
-          if (number[i] === number[c]) {
-              cMaior += 1;
-          }
-      }
-      if (cMaior >= 3) {
-          return 'não é possível gerar um número de telefone com esses valores';
-      }
-  } 
+    }
+    if (cMaior >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
   if (number.length === 11) {
-      for (let i = 0; i < number.length; i += 1) {
-          if (nNumber[0] === undefined) {
-              nNumber += '(';
-              nNumber += number[i];
-          } else if (nNumber.length < 3) {
-              nNumber += number[i];
-              nNumber += ') ';
-          } else if (nNumber.length < 9) {
-              nNumber += number[i];
-          } else if (nNumber.length === 9) {
-              nNumber += `${number[i]}-`;
-          } else if (i < number.length) {
-              nNumber += number[i];
-          }
+    for (let i = 0; i < number.length; i += 1) {
+      if (nNumber[0] === undefined) {
+        nNumber += '(';
+        nNumber += number[i];
+      } else if (nNumber.length < 3) {
+        nNumber += number[i];
+        nNumber += ') ';
+      } else if (nNumber.length < 9) {
+        nNumber += number[i];
+      } else if (nNumber.length === 9) {
+        nNumber += `${number[i]}-`;
+      } else if (i < number.length) {
+        nNumber += number[i];
       }
-      return nNumber;
+    }
+    return nNumber;
   }
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let sumAB = lineA + lineB > lineC;
+  let sumAC = lineA + lineC > lineB;
+  let sumBC = lineB + lineC > lineA;
+
+  return sumAB && sumAC && sumBC;
 }
 
 // Desafio 13
