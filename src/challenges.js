cb7a0353f442026093695ee62d4371543106f447
Samuel <samuelvanguarda@gmail.com>
2020-11-05 12:43:20 -0300

    Completando Requisitos Obrigatorios

--- src/challenges.js
+++ src/challenges.js
@@ -6,14 +6,14 @@ function compareTrue(bool1, bool2) {
     return false;
   }
 }
-console.log(compareTrue(true, true));
+
 // Desafio 2
 function calcArea(base, height) {
   let areaTriangulo;
   areaTriangulo = (base * height) / 2;
   return areaTriangulo;
 }
-console.log(calcArea(4,6));
+
 // Desafio 3
 //Bibliografia: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254#:~:text=O%20m%C3%A9todo%20split()%20divide,das%20substrings%20resultantes%20no%20array.
 function splitSentence(string) {
@@ -21,7 +21,7 @@ function splitSentence(string) {
   dividido = string.split(" ");
   return dividido;
 }
-console.log(splitSentence("ola povo"));
+
 // Desafio 4
 function concatName(array) {
   let invertido = [];
@@ -30,13 +30,13 @@ function concatName(array) {
   }
   return(invertido[invertido.length-1] + ", " + invertido[0])
 } 
-console.log(concatName(['pedro', 'joão' , 'rosa', 'maria']));
+
 // Desafio 5
 function footballPoints(wins, ties) {
   let points = (wins *3) + ties;
   return points;
 } 
-console.log(footballPoints(3, 2));
+
 // Desafio 6
 function highestCount(arrayNumeros) {
   let maior = 0;
@@ -53,7 +53,7 @@ function highestCount(arrayNumeros) {
   }
   return vezes;
 }
-  console.log(highestCount([3, 2, 3, 1, 2]));
+
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distancia1 = Math.abs(cat1 - mouse);
@@ -65,10 +65,9 @@ function catAndMouse(mouse, cat1, cat2) {
   }else{
     gatoVencedor = "cat1";
   }
-  
   return gatoVencedor;
 }
-console.log(catAndMouse(2, 3, 3))
+
 // Desafio 8
 function fizzBuzz(array) {
   let divisiveis = [];
@@ -85,7 +84,7 @@ function fizzBuzz(array) {
   }
   return divisiveis;
 }
-console.log(fizzBuzz([2, 3, 15, 30]));
+
 // Desafio 9
 function encode(stringQualquer) {
   let concatenado = '';
@@ -106,7 +105,7 @@ function encode(stringQualquer) {
   }
   return concatenado;
 }
-console.log(encode('samuel ramos campos'))
+
 function decode(codigo) {
   let transcrito = '';
   for(let i = 0; i < codigo.length; i++){
@@ -126,7 +125,7 @@ function decode(codigo) {
   }
   return transcrito;
 }
-console.log((decode('s1m52l r1m4s c1mp4s')))
+
 // Desafio 10
 function techList(tecnologias, name) {
   let objetos = [];
@@ -143,7 +142,7 @@ function techList(tecnologias, name) {
   }
   return objetos;
 }
-console.log(techList(['css', 'java', 'html', 'python'], "samuel"));
+
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
