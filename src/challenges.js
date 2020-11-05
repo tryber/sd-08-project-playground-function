d8cfacd8dce922c86fb19a93ba51fe7b7f1509d1
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-05 10:06:36 -0300

    add challange 9

--- src/challenges.js
+++ src/challenges.js
@@ -66,8 +66,21 @@ function catAndMouse() {
   }
 }
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+let result = [];
+for (let index = 0; index < numbers.length; index += 1) {
+if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+  result.push("fizzBuzz")
+  
+} else if (numbers[index] % 5 === 0) {
+  result.push("buzz")
+} else if (numbers[index] % 3 === 0) {
+result.push("fizz")
+} else {
+result.push("bug!")
+}
+}
+return result
 }
 
 // Desafio 9
