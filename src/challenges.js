695e3b761bf25d98221c5cf031e2b08fd30f4ba9
Thais Quintela <thais.s.quintela@gmail.com>
2020-11-04 23:08:53 -0300

    Requisito 11 pela milesima vez...

--- src/challenges.js
+++ src/challenges.js
@@ -145,8 +145,6 @@ function generatePhoneNumber(numbers) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }
-  }
-  else {
     return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
   }
 }
