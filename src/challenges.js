135921babe8b474f338f745d0f3722d57b4e6768
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-04 23:15:59 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -16,32 +16,37 @@ function calcArea(base, height) {
   return area;
 }
 
-
 // Desafio 3
 function splitSentence(mySentence) {
   // seu código aqui
-  let resultado = mySentence.split(' ');
+  let resultado = mySentence.split(" ");
   return resultado;
 }
 
 // Desafio 4
 function concatName(parametro) {
   // seu código aqui
-  return `${parametro[parametro.length -1]}, ${parametro[0]}`;
+  return `${parametro[parametro.length - 1]}, ${parametro[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  let totalPoints = (wins * 3) + (ties * 1)
+  let totalPoints = wins * 3 + ties * 1;
   return totalPoints;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numeros) {
   // seu código aqui
-}
 
+  let valorMaisAlto = Math.max(numeros);
+  var quantidade = 0;
+  for (let index = 0; index <= numeros.length; index += 1) {
+    if (numeros[index] == valorMaisAlto) quantidade += 1;
+  }
+  return quantidade;
+}
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
@@ -50,24 +55,22 @@ function catAndMouse() {
 // Desafio 8
 function fizzBuzz(meuArray) {
   // seu código aqui
-  var entradaDoUsuario =[meuArray];
+  var entradaDoUsuario = [meuArray];
 
-  for (let i = 1; i < entradaDoUsuario.length;i++){
-    if (entradaDoUsuario[i] % 3 === 0){
-     return console.log('fizz');
-    } else if(entradaDoUsuario[i] % 5 === 0){
-      return console.log('buzz');
-    } else if (entradaDoUsuario[i] % 3 === 0 && entradaDoUsuario[i] % 5 === 0){
-      return console.log('fizz buzz');
+  for (let i = 1; i < entradaDoUsuario.length; i++) {
+    if (entradaDoUsuario[i] % 3 === 0) {
+      return console.log("fizz");
+    } else if (entradaDoUsuario[i] % 5 === 0) {
+      return console.log("buzz");
+    } else if (entradaDoUsuario[i] % 3 === 0 && entradaDoUsuario[i] % 5 === 0) {
+      return console.log("fizz buzz");
     } else {
-      return console.log('bug');
+      return console.log("bug");
     }
   }
-
 }
 
 console.log(fizzBuzz(2, 15, 7, 9, 45));
-
 
 // Desafio 9
 function encode() {
