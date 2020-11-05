320189be84e404340a578557e5184b08a9b70d26
Pedro Piquini <piquini.pedro@gmail.com>
2020-11-05 14:44:18 -0300

    quinto exercicio feito e correção no quarto

--- src/challenges.js
+++ src/challenges.js
@@ -21,12 +21,12 @@ function splitSentence(frase) {
 
 // Desafio 4
 function concatName(nome) {
-  return nome.lenght-1 + "," + nome[0]
+  return nome[nome.lenght-1] + "," + nome[0]
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  return (wins * 3) + ties
 }
 
 // Desafio 6
