d43e54294867e1b03bd34364c7e7b8a2028a35ff
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-05 00:28:40 -0300

    Atualizando

--- src/challenges.js
+++ src/challenges.js
@@ -71,11 +71,11 @@ function fizzBuzz(entradaDoUsuario) {
   var meuArray = [];
   for (let i = 0; i < entradaDoUsuario.length; i++) {
       meuArray[i] = "";
-    if (entradaDoUsuario[i] % 3 == 0) {
+    if (entradaDoUsuario[i] % 3 === 0) {
       meuArray[i] = "fizz";
-    } else if (entradaDoUsuario[i] % 5 == 0) {
+    } else if (entradaDoUsuario[i] % 5 === 0) {
       meuArray[i] = "buzz";
-    } else if (entradaDoUsuario[i] % 3 == 0 && entradaDoUsuario[i] % 5 == 0) {
+    } else if (entradaDoUsuario[i] % 3 === 0 && entradaDoUsuario[i] % 5 === 0) {
       meuArray[i] = "fizzBuzz";
     } else {
       meuArray[i] = "bug!";
