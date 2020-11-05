21a028e0deca48a92720935df553cba8c2931a7d
Caio Menezes Rocha <caiomzrocha@gmail.com>
2020-11-05 03:36:51 -0300

    Removido console.logs desafio 11 - generatePhoneNumber

--- src/challenges.js
+++ src/challenges.js
@@ -244,8 +244,6 @@ function generatePhoneNumber(numbers) {
 
   return phoneNumber;
 }
-console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]));
-console.log(generatePhoneNumber([0,2,3,4,4,2,7,8,9,9,4]));
 
 // Desafio 12
 function triangleCheck() {
