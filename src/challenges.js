d057537f004e98fb2d3d65c3da910a7cfaefd9c8
Akirakiraa <andretaki@2gmail.com>
2020-11-15 05:50:07 -0300

    ex11 return position

--- src/challenges.js
+++ src/challenges.js
@@ -155,14 +155,16 @@ function generatePhoneNumber(array) {
             if (array[pos1] === array[pos2]) {
                 count3++;
                 if (count3 >= 3) {
-                    return "não é possível gerar um número de telefone com esses valores2"
+                    return "não é possível gerar um número de telefone com esses valores2";
+                } else {
+                    return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
                 }
             }
         }
-
     }
-    return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+
 }
+
 
 // Desafio 12
 function triangleCheck(ladoA, ladoB, ladoC) {
