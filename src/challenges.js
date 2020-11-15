b46ea7f1aeaea986d1946804074baf85c2fc2530
Akirakiraa <andretaki@2gmail.com>
2020-11-15 05:21:20 -0300

    ex11 EU TAVA ESCREVENDO TELEFOEgit add .git add .

--- src/challenges.js
+++ src/challenges.js
@@ -149,7 +149,7 @@ function generatePhoneNumber(array) {
     }
     for (let pos1 = 0; pos1 < array.length; pos1++) {
         if (array[pos1] < 0 || array[pos1] > 9) {
-            return "não é possível gerar um número de telefoe com esses valores";
+            return "não é possível gerar um número de telefone com esses valores";
         }
         for (let pos2 = 0; pos2 < array.length; pos2++) {
             if (array[pos1] === array[pos2]) {
@@ -158,7 +158,7 @@ function generatePhoneNumber(array) {
         }
     }
     if (count3 >= 3) {
-        return "não é possível gerar um número de telefoe com esses valores"
+        return "não é possível gerar um número de telefone com esses valores"
     }
     return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
