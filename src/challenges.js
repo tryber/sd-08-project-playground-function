0e7a264a694c27464ff8e28e6b8e6ef58bbd899e
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-20 16:31:20 -0300

    :sparkles: Finalizado Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -85,12 +85,44 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode(string) {}
-
-function decode() {
-  // seu código aqui
+function encode(string) {
+  string = string.split('')
+  for(let index = 0; index < string.length; index += 1) {
+    if(string[index] === 'a') {
+      string[index] = 1;
+    }else if(string[index] === 'e') {
+      string[index] = 2;
+    }else if(string[index] === 'i') {
+      string[index] = 3;
+    }else if(string[index] === 'o') {
+      string[index] = 4;
+    }else if(string[index] === 'u') {
+      string[index] = 5;
+    }
+  }
+  string = string.join('');
+  return string;
 }
 
+function decode(string) {
+  string = string.split('')
+  for(let index = 0; index < string.length; index += 1) {
+    if(string[index] === '1') {
+      string[index] = 'a';
+    }else if(string[index] === '2') {
+      string[index] = 'e';
+    }else if(string[index] === '3') {
+      string[index] = 'i';
+    }else if(string[index] === '4') {
+      string[index] = 'o';
+    }else if(string[index] === '5') {
+      string[index] = 'u';
+    }
+  }
+  string = string.join('');
+  return string;
+}
+console.log(decode('h3 th2r2!'));
 // Desafio 10
 function techList() {
   // seu código aqui
